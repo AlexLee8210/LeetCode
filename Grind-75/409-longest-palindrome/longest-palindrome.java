5166b235d365353e10f7863fcb303bab07792d75
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-27 16:39:51 -0500

    Time: 7 ms (54.18%) | Memory: 41.6 MB (60.76%) - LeetSync

--- Grind-75/409-longest-palindrome/longest-palindrome.java
+++ Grind-75/409-longest-palindrome/longest-palindrome.java
@@ -4,19 +4,17 @@ class Solution {
         for (char c : s.toCharArray()) {
             map.put(c, map.getOrDefault(c, 0) + 1);
         }
-
+        boolean odd = false;
         int total = 0;
         for (char c : map.keySet()) {
             int n = map.get(c);
             if (n % 2 == 0) {
                 total += n;
             } else {
-                if (total % 2 == 0) {
-                    total++;
-                }
+                odd = true;
                 total += n - 1;
             }
         }
-        return total;
+        return total + (odd ? 1 : 0);
     }
 }
