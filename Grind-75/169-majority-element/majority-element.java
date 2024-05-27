2af00088b2ebec6147d4508d50be7550964c1c6e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-27 17:15:37 -0500

    Time: 2 ms (77.18%) | Memory: 53.2 MB (16.84%) - LeetSync

--- Grind-75/169-majority-element/majority-element.java
+++ Grind-75/169-majority-element/majority-element.java
@@ -1,13 +1,18 @@
 class Solution {
     public int majorityElement(int[] nums) {
-        HashMap<Integer, Integer> map = new HashMap<>();
-        for (int i = 0; i < nums.length; i++) {
-            int n = map.getOrDefault(nums[i], 0) + 1;
-            map.put(nums[i], n);
-            if (n > nums.length / 2) {
-                return nums[i];
+        int majority = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] == majority) {
+                count++;
+            } else {
+                count--;
+                if (count <= 0) {
+                    majority = nums[i];
+                    count = 1;
+                }
             }
         }
-        return -1;
+        return majority;
     }
 }
