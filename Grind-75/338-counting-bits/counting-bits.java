4d053b86ae8f4a57837f9a6cb2174fd95466bebc
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-28 10:45:36 -0500

    Time: 2 ms (94.67%) | Memory: 46.3 MB (75.18%) - LeetSync

---
+++ Grind-75/338-counting-bits/counting-bits.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int[] countBits(int n) {
+        int[] bin = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            bin[i] = bin[i / 2] + (i % 2 == 1 ? 1 : 0);
+        }
+        return bin;
+    }
+}
