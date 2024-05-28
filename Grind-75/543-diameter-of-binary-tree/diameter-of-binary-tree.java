d2756974e73ef0592f8a19e5738c3f903458e9ec
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-27 19:28:38 -0500

    Time: 0 ms (100.00%) | Memory: 44.8 MB (44.23%) - LeetSync

--- Grind-75/543-diameter-of-binary-tree/diameter-of-binary-tree.java
+++ Grind-75/543-diameter-of-binary-tree/diameter-of-binary-tree.java
@@ -15,35 +15,19 @@
  */
 class Solution {
     public int diameterOfBinaryTree(TreeNode root) {
-        return diam(root, new HashMap<>());
+        int[] ans = new int[1];
+        height(root, ans);
+        return ans[0];
     }
 
-    private int diam(TreeNode node, HashMap<TreeNode, Integer> heightMap) {
+    private int height(TreeNode node, int[] diam) {
         if (node == null) return 0;
-        int diameter = height(node.left, heightMap) + height(node.right, heightMap);
-        return Math.max(diameter, Math.max(diam(node.left, heightMap), diam(node.right, heightMap)));
-    }
+        int lHeight = height(node.left, diam);
+        int rHeight = height(node.right, diam);
+        int diameter = lHeight + rHeight;
+        diam[0] = Math.max(diameter, diam[0]);
 
-    // private int diamHelper(TreeNode cur, HashMap<TreeNode, Integer> heightMap) {
-    //     if (cur.left == null && cur.right == null) {
-    //         return 0;
-    //     } else if (cur.left == null) {
-    //         return height(cur.right, heightMap);
-    //     } else if (cur.right == null) {
-    //         return height(cur.left, heightMap);
-    //     }
-    //     int maxDiam = Math.max(diamHelper(cur.left, heightMap), diamHelper(cur.right, heightMap));
-    //     return Math.max(maxDiam, height(cur.left, heightMap) + height(cur.right, heightMap));
-    // }
-
-    private int height(TreeNode cur, HashMap<TreeNode, Integer> memo) {
-        if (cur == null) {
-            return 0;
-        } else if (memo.containsKey(cur)) {
-            return memo.get(cur);
-        }
-        int height = Math.max(height(cur.left, memo), height(cur.right, memo)) + 1;
-        memo.put(cur, height);
+        int height = Math.max(lHeight, rHeight) + 1;
         return height;
     }
 }
