700d9f2d659946a4eaf914bec768c802cacc6cad
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-29 09:44:01 -0500

    Time: 1 ms (75.47%) | Memory: 41.4 MB (59.28%) - LeetSync

---
+++ Grind-75/14-longest-common-prefix/longest-common-prefix.java
@@ -0,0 +1,16 @@
+class Solution {
+    public String longestCommonPrefix(String[] strs) {
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        while (index < strs[0].length()) {
+            char cur = strs[0].charAt(index);
+            for (int i = 1; i < strs.length; i++) {
+                if (strs[i].length() <= index) return sb.toString();
+                if (strs[i].charAt(index) != cur) return sb.toString();
+            }
+            sb.append(cur);
+            index++;
+        }
+        return sb.toString();
+    }
+}
