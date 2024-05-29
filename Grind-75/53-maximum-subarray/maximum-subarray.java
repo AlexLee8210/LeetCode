1704cfcf7a581cde8ab9ba1f78a0c43eff5b12b7
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-29 14:36:16 -0500

    Time: 1 ms (99.47%) | Memory: 57 MB (49.94%) - LeetSync

--- Grind-75/53-maximum-subarray/maximum-subarray.java
+++ Grind-75/53-maximum-subarray/maximum-subarray.java
@@ -3,11 +3,9 @@ class Solution {
         int sum = 0;
         int max = Integer.MIN_VALUE;
         for (int i = 0; i < nums.length; i++) {
-            if (sum < 0) {
-                sum = 0;
-            }
             sum += nums[i];
-            max = Math.max(max, sum);
+            max = max > sum ? max : sum;
+            if (sum < 0) sum = 0;
         }
         return max;
     }
