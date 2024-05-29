f45db31f64e2ea87b5fd5ccb044630e410549003
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-29 10:12:54 -0500

    Time: 4 ms (87.05%) | Memory: 68.4 MB (43.63%) - LeetSync

--- Grind-75/234-palindrome-linked-list/palindrome-linked-list.java
+++ Grind-75/234-palindrome-linked-list/palindrome-linked-list.java
@@ -11,19 +11,32 @@
 class Solution {
     public boolean isPalindrome(ListNode head) {
         ListNode slow = head, fast = head;
-        Stack<Integer> stack = new Stack<>();
         while (fast != null && fast.next != null) {
-            stack.push(slow.val);
             slow = slow.next;
             fast = fast.next.next;
         }
         // ignore middle
         if (fast != null) slow = slow.next;
+        ListNode rev = reverse(slow);
         // slow at middle
-        while (slow != null && !stack.isEmpty()) {
-            if (slow.val != stack.pop()) return false;
-            slow = slow.next;
+        while (rev != null) {
+            if (rev.val != head.val) return false;
+            rev = rev.next;
+            head = head.next;
         }
         return true;
+    }
+
+    private ListNode reverse(ListNode node) {
+        ListNode prev = null;
+        ListNode cur = node;
+        ListNode next;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = prev;
+            prev = cur;
+            cur = next;
+        }
+        return prev;
     }
 }
