40b73588ab2ba2a5098c06e6bda39be375897116
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-29 11:42:28 -0500

    Time: 4 ms (100.00%) | Memory: 44 MB (42.39%) - LeetSync

--- Grind-75/9-palindrome-number/palindrome-number.java
+++ Grind-75/9-palindrome-number/palindrome-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public boolean isPalindrome(int x) {
         if (x < 0) return false;
-        if (x < 10 || x == 0) return true;
-        if (x % 10 == 0) return false;
-        // if (x % 11 == 0 || x < 10) return true;
+        else if (x < 10 || x == 0) return true;
+        else if (x % 10 == 0) return false;
+        else if (x < 1000 && x % 11 == 0) return true;
         int rev = 0;
         int n = x;
         while (x > 0) {
