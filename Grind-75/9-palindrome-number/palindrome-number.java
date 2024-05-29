eb76a6ffacc66b537cdcc8df85645e17596fc118
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-29 11:46:35 -0500

    Time: 4 ms (100.00%) | Memory: 43.6 MB (81.85%) - LeetSync

--- Grind-75/9-palindrome-number/palindrome-number.java
+++ Grind-75/9-palindrome-number/palindrome-number.java
@@ -3,7 +3,7 @@ class Solution {
         if (x < 0) return false;
         else if (x < 10 || x == 0) return true;
         else if (x % 10 == 0) return false;
-        // else if (x < 1000 && x % 11 == 0) return true;
+        else if (x < 1000 && x % 11 == 0) return true;
         int rev = 0;
         while (x > rev) {
             rev = rev * 10 + x % 10;
