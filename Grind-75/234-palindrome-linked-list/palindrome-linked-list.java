aba7cd1e9d25feb378ccfd54373ea983893bcffb
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-29 10:02:17 -0500

    Time: 10 ms (24.47%) | Memory: 57.4 MB (84.80%) - LeetSync

---
+++ Grind-75/234-palindrome-linked-list/palindrome-linked-list.java
@@ -0,0 +1,29 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        ListNode slow = head, fast = head;
+        Stack<Integer> stack = new Stack<>();
+        while (fast != null && fast.next != null) {
+            stack.push(slow.val);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        // ignore middle
+        if (fast != null) slow = slow.next;
+        // slow at middle
+        while (slow != null && !stack.isEmpty()) {
+            if (slow.val != stack.pop()) return false;
+            slow = slow.next;
+        }
+        return true;
+    }
+}
