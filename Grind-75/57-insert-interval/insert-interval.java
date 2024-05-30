acc32f62f67e3684097861ea637b62f739eb2611
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-30 14:44:41 -0500

    Time: 1 ms (98.57%) | Memory: 45.2 MB (7.02%) - LeetSync

--- Grind-75/57-insert-interval/insert-interval.java
+++ Grind-75/57-insert-interval/insert-interval.java
@@ -5,15 +5,16 @@ class Solution {
         while (index < intervals.length && newInterval[0] > intervals[index][1]) {
             res.add(intervals[index++]);
         }
-        if (index == intervals.length) {
-            res.add(newInterval);
-            return res.toArray(new int[res.size()][]);
-        } else if (newInterval[1] >= intervals[index][0]) {
-            newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
-            index++;
-        }
+        // if (index == intervals.length) {
+        //     res.add(newInterval);
+        //     return res.toArray(new int[res.size()][]);
+        // } else if (newInterval[1] >= intervals[index][0]) {
+        //     newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
+        //     newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
+        //     index++;
+        // }
         while (index < intervals.length && newInterval[1] >= intervals[index][0]) {
+            newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
             newInterval[1] = Math.max(intervals[index++][1], newInterval[1]);
         }
         res.add(newInterval);
