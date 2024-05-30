f3e15ac4d7b6760d23d45041ac042b7cb451e16f
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-05-30 14:45:32 -0500

    Time: 1 ms (98.57%) | Memory: 44.9 MB (21.45%) - LeetSync

--- Grind-75/57-insert-interval/insert-interval.java
+++ Grind-75/57-insert-interval/insert-interval.java
@@ -2,22 +2,18 @@ class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
         ArrayList<int[]> res = new ArrayList<>();
         int index = 0;
+        // before insert
         while (index < intervals.length && newInterval[0] > intervals[index][1]) {
             res.add(intervals[index++]);
         }
-        // if (index == intervals.length) {
-        //     res.add(newInterval);
-        //     return res.toArray(new int[res.size()][]);
-        // } else if (newInterval[1] >= intervals[index][0]) {
-        //     newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
-        //     newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
-        //     index++;
-        // }
+        // merge insert
         while (index < intervals.length && newInterval[1] >= intervals[index][0]) {
             newInterval[0] = Math.min(intervals[index][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[index++][1], newInterval[1]);
+            newInterval[1] = Math.max(intervals[index][1], newInterval[1]);
+            index++;
         }
         res.add(newInterval);
+        // after insert
         while (index < intervals.length) {
             res.add(intervals[index++]);
         }
