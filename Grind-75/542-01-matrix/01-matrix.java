af6c3b83f72a84431d9ebfa903841d2777118264
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-06-02 10:51:55 -0500

    Time: 6 ms (99.43%) | Memory: 47.7 MB (24.16%) - LeetSync

--- Grind-75/542-01-matrix/01-matrix.java
+++ Grind-75/542-01-matrix/01-matrix.java
@@ -1,44 +1,22 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
         int INF = mat.length + mat[0].length;
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
         for (int r = 0; r < mat.length; r++) {
             for (int c = 0; c < mat[0].length; c++) {
-                if (mat[r][c] == 0) {
-                    q.add(new Pair<>(r, c));
-                } else {
-                    mat[r][c] = INF;
-                }
+                if (mat[r][c] == 0) continue;
+                int top = INF, left = INF;
+                if (r > 0) top = mat[r - 1][c];
+                if (c > 0) left = mat[r][c - 1];
+                mat[r][c] = Math.min(top, left) + 1;
             }
         }
-        while (!q.isEmpty()) {
-            Pair<Integer, Integer> pos = q.poll();
-            int r = pos.getKey(), c = pos.getValue();
-            int comp = mat[r][c] + 1;
-
-            if (r > 0) {
-                if (mat[r - 1][c] > comp) {
-                    mat[r - 1][c] = comp;
-                    q.add(new Pair<>(r - 1, c));
-                }
-            }
-            if (r < mat.length - 1) {
-                if (mat[r + 1][c] > comp) {
-                    mat[r + 1][c] = comp;
-                    q.add(new Pair<>(r + 1, c));
-                }
-            }
-            if (c > 0) {
-                if (mat[r][c - 1] > comp) {
-                    mat[r][c - 1] = comp;
-                    q.add(new Pair<>(r, c - 1));
-                }
-            }
-            if (c < mat[0].length - 1) {
-                if (mat[r][c + 1] > comp) {
-                    mat[r][c + 1] = comp;
-                    q.add(new Pair<>(r, c + 1));
-                }
+        for (int r = mat.length - 1; r >= 0; r--) {
+            for (int c = mat[0].length - 1; c >= 0; c--) {
+                if (mat[r][c] == 0) continue;
+                int bottom = INF, right = INF;
+                if (r < mat.length - 1) bottom = mat[r + 1][c];
+                if (c < mat[0].length - 1) right = mat[r][c + 1];
+                mat[r][c] = Math.min(mat[r][c], Math.min(bottom, right) + 1);
             }
         }
         return mat;
