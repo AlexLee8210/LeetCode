c8d7518e45f7738e769def95922e9044a1681a4d
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-06-02 11:21:51 -0500

    Time: 31 ms (55.42%) | Memory: 53.6 MB (38.60%) - LeetSync

--- Grind-75/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ Grind-75/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,22 +1,18 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
         int[][] ans = new int[k][2];
-        // long[] dist = new long[points.length];
-        TreeMap<Long, LinkedList<int[]>> dist = new TreeMap<>();
+        PriorityQueue<int[]> pq = new PriorityQueue<>(k + 1, (p1, p2) -> 
+            (p2[0] * p2[0] + p2[1] * p2[1]) - (p1[0] * p1[0] + p1[1] * p1[1])
+        );
         for (int[] point : points) {
-            long d = (long)Math.pow(point[0], 2) + (long)Math.pow(point[1], 2);
-            LinkedList<int[]> list = dist.getOrDefault(d, new LinkedList<>());
-            list.add(point);
-            dist.put(d, list);
+            pq.add(point);
+            if (pq.size() > k) {
+                pq.poll();
+            }
         }
         int idx = 0;
-        for (Long d : dist.keySet()) {
-            if (idx >= k)  break;
-            LinkedList<int[]> list = dist.get(d);
-            for (int[] point : list) {
-                if (idx >= k)  break;
-                ans[idx++] = point;
-            }
+        while (!pq.isEmpty()) {
+            ans[idx++] = pq.poll();
         }
         return ans;
     }
