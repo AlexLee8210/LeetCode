2d39543f1b66a32ead2407d8f089fd62600f7030
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-06-02 10:52:48 -0500

    Time: 6 ms (99.43%) | Memory: 47.4 MB (43.09%) - LeetSync

--- Grind-75/542-01-matrix/01-matrix.java
+++ Grind-75/542-01-matrix/01-matrix.java
@@ -1,8 +1,9 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int INF = mat.length + mat[0].length;
-        for (int r = 0; r < mat.length; r++) {
-            for (int c = 0; c < mat[0].length; c++) {
+        int m = mat.length, n = mat[0].length;
+        int INF = m + n;
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
                 if (mat[r][c] == 0) continue;
                 int top = INF, left = INF;
                 if (r > 0) top = mat[r - 1][c];
@@ -10,12 +11,12 @@ class Solution {
                 mat[r][c] = Math.min(top, left) + 1;
             }
         }
-        for (int r = mat.length - 1; r >= 0; r--) {
-            for (int c = mat[0].length - 1; c >= 0; c--) {
+        for (int r = m - 1; r >= 0; r--) {
+            for (int c = n - 1; c >= 0; c--) {
                 if (mat[r][c] == 0) continue;
                 int bottom = INF, right = INF;
-                if (r < mat.length - 1) bottom = mat[r + 1][c];
-                if (c < mat[0].length - 1) right = mat[r][c + 1];
+                if (r < m - 1) bottom = mat[r + 1][c];
+                if (c < n - 1) right = mat[r][c + 1];
                 mat[r][c] = Math.min(mat[r][c], Math.min(bottom, right) + 1);
             }
         }
