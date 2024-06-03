fbf848dfcfa6e59cdf0fbce3ea9f92ea4c0c3c78
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-06-03 17:04:41 -0500

    Time: 8 ms (21.71%) | Memory: 45.3 MB (38.65%) - LeetSync

--- Grind-75/207-course-schedule/course-schedule.java
+++ Grind-75/207-course-schedule/course-schedule.java
@@ -1,36 +1,67 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegree = new int[numCourses];
         HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
         for (int[] course : prerequisites) {
-            HashSet<Integer> prereq = map.getOrDefault(course[0], new HashSet<>());
-            prereq.add(course[1]);
-            map.put(course[0], prereq);
+            HashSet<Integer> prereq = map.getOrDefault(course[1], new HashSet<>());
+            prereq.add(course[0]);
+            map.put(course[1], prereq);
+            indegree[course[0]]++;
         }
-        HashSet<Integer> canTake = new HashSet<>();
+        // System.out.println(map);
+        // System.out.println(Arrays.toString(indegree));
+        Queue<Integer> q = new LinkedList<>();
         for (int i = 0; i < numCourses; i++) {
-            if (!map.containsKey(i)) {
-                canTake.add(i);
+            if (indegree[i] == 0) {
+                q.offer(i);
             }
         }
-        
-        while (canTake.size() != numCourses) {
-            int prevSize = canTake.size();
-            for (int course : map.keySet()) {
-                if (canTake.contains(course)) continue;
-                HashSet<Integer> prereqs = map.get(course);
-                boolean valid = true;
-                for (int pre : prereqs) {
-                    if (!canTake.contains(pre)) {
-                        valid = false;
-                        break;
-                    }
-                }
-                if (valid) {
-                    canTake.add(course);
+        while (!q.isEmpty()) {
+            int course = q.poll();
+            HashSet<Integer> postreqs = map.get(course);
+            if (postreqs == null) continue;
+            for (int postreq : postreqs) {
+                indegree[postreq]--;
+                if (indegree[postreq] == 0) {
+                    q.offer(postreq);
                 }
             }
-            if (prevSize == canTake.size()) return false;
+        }
+        for (int i = 0; i < numCourses; i++) {
+            if (indegree[i] > 0) return false;
         }
         return true;
+        // HashMap<Integer, HashSet<Integer>> map = new HashMap<>();
+        // for (int[] course : prerequisites) {
+        //     HashSet<Integer> prereq = map.getOrDefault(course[0], new HashSet<>());
+        //     prereq.add(course[1]);
+        //     map.put(course[0], prereq);
+        // }
+        // HashSet<Integer> canTake = new HashSet<>();
+        // for (int i = 0; i < numCourses; i++) {
+        //     if (!map.containsKey(i)) {
+        //         canTake.add(i);
+        //     }
+        // }
+        
+        // while (canTake.size() != numCourses) {
+        //     int prevSize = canTake.size();
+        //     for (int course : map.keySet()) {
+        //         if (canTake.contains(course)) continue;
+        //         HashSet<Integer> prereqs = map.get(course);
+        //         boolean valid = true;
+        //         for (int pre : prereqs) {
+        //             if (!canTake.contains(pre)) {
+        //                 valid = false;
+        //                 break;
+        //             }
+        //         }
+        //         if (valid) {
+        //             canTake.add(course);
+        //         }
+        //     }
+        //     if (prevSize == canTake.size()) return false;
+        // }
+        // return true;
     }
 }
