9df9c79f554686d476a5496f99595566df3db479
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-06-03 12:08:58 -0500

    Time: 1 ms (88.47%) | Memory: 45.2 MB (9.60%) - LeetSync

--- Grind-75/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
+++ Grind-75/102-binary-tree-level-order-traversal/binary-tree-level-order-traversal.java
@@ -15,29 +15,45 @@
  */
 class Solution {
     public List<List<Integer>> levelOrder(TreeNode root) {
-        Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
-        q.offer(new Pair<>(root, 0));
-        List<List<Integer>> res = new ArrayList<>();
-        List<Integer> cur = new ArrayList<>();
-        int curLevel = 0;
-
+        Queue<TreeNode> q = new LinkedList<>();
+        q.offer(root);
+        ArrayList<List<Integer>> res = new ArrayList<>();
         while (!q.isEmpty()) {
-            Pair<TreeNode, Integer> pair = q.poll();
-            TreeNode node = pair.getKey();
-            int level = pair.getValue();
-            if (node == null) {
-                continue;
+            int qSize = q.size();
+            ArrayList<Integer> cur = new ArrayList<>(qSize);
+            for (int i = 0; i < qSize; i++) {
+                TreeNode node = q.poll();
+                if (node == null) continue;
+                cur.add(node.val);
+                q.offer(node.left);
+                q.offer(node.right);
             }
-            q.offer(new Pair<>(node.left, level + 1));
-            q.offer(new Pair<>(node.right, level + 1));
-            if (level > curLevel) {
-                res.add(cur);
-                cur = new ArrayList<>();
-                curLevel++;
-            }
-            cur.add(node.val);
+            if (cur.size() > 0) res.add(cur);
         }
-        if (cur.size() > 0) res.add(cur);
         return res;
+        // Queue<Pair<TreeNode, Integer>> q = new LinkedList<>();
+        // q.offer(new Pair<>(root, 0));
+        // List<List<Integer>> res = new ArrayList<>();
+        // List<Integer> cur = new ArrayList<>();
+        // int curLevel = 0;
+
+        // while (!q.isEmpty()) {
+        //     Pair<TreeNode, Integer> pair = q.poll();
+        //     TreeNode node = pair.getKey();
+        //     int level = pair.getValue();
+        //     if (node == null) {
+        //         continue;
+        //     }
+        //     q.offer(new Pair<>(node.left, level + 1));
+        //     q.offer(new Pair<>(node.right, level + 1));
+        //     if (level > curLevel) {
+        //         res.add(cur);
+        //         cur = new ArrayList<>();
+        //         curLevel++;
+        //     }
+        //     cur.add(node.val);
+        // }
+        // if (cur.size() > 0) res.add(cur);
+        // return res;
     }
 }
