140e21a20ef99abc40efdabb83feed825b495afa
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-06-10 16:21:54 -0500

    Time: 0 ms (100.00%) | Memory: 41.9 MB (42.30%) - LeetSync

--- Grind-75/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ Grind-75/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,9 +3,8 @@ class Solution {
         int l = 0, r = nums.length - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
-            System.out.println(nums[m]);
             if (nums[m] == target) return m;
-            else if (isSorted(nums, l, m)) {
+            else if (nums[l] <= nums[m]) {
                 if (nums[l] <= target && target < nums[m]) {
                     r = m - 1;
                 } else {
@@ -20,9 +19,5 @@ class Solution {
             }
         }
         return -1;
-    }
-
-    private boolean isSorted(int[] nums, int l, int r) {
-        return nums[l] <= nums[r];
     }
 }
