f05447c4e46d4af2dd33d7741f65a0cec91e9bc2
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-09 09:48:55 -0500

    Time: 27 ms (92.21%) | Memory: 47.3 MB (77.18%) - LeetSync

---
+++ Grind-75/721-accounts-merge/accounts-merge.java
@@ -0,0 +1,81 @@
+class Solution {
+
+    private class UnionFind {
+        private int[] parent;
+        private int[] rank;
+
+        public UnionFind(int n) {
+            parent = new int[n];
+            rank = new int[n];
+
+            for (int i = 0; i < n; i++) {
+                parent[i] = i;
+            }
+        }
+
+        public int find(int a) {
+            if (parent[a] == a) {
+                return a;
+            }
+            int par = find(parent[a]);
+            parent[a] = par;
+            return par;
+        }
+
+        public boolean union(int a, int b) {
+            int root1 = find(a);
+            int root2 = find(b);
+            if (root1 == root2) {
+                return false;
+            } else if (rank[root2] > rank[root1]) {
+                parent[root1] = root2;
+                rank[root2] += rank[root1];
+            } else {
+                parent[root2] = root1;
+                rank[root1] += rank[root2];
+            }
+            return true;
+        }
+    }
+
+    public List<List<String>> accountsMerge(List<List<String>> accounts) {
+        UnionFind uf = new UnionFind(accounts.size());
+
+        String[] names = new String[accounts.size()];
+
+        HashMap<String, Integer> emailToId = new HashMap<>();
+        for (int i = 0; i < accounts.size(); i++) {
+            List<String> account = accounts.get(i);
+            names[i] = account.get(0);
+            for (int j = 1; j < account.size(); j++) {
+                String email = account.get(j);
+                if (emailToId.containsKey(email)) {
+                    uf.union(i, emailToId.get(email));
+                } else {
+                    emailToId.put(email, i);
+                }
+            }
+        }
+
+        List<List<String>> result = new ArrayList<>();
+        HashMap<Integer, ArrayList<String>> idToEmails = new HashMap<>();
+        for (String email : emailToId.keySet()) {
+            int id = uf.find(emailToId.get(email));
+            if (idToEmails.containsKey(id)) {
+                ArrayList<String> list = idToEmails.get(id);
+                list.add(email);
+            } else {
+                ArrayList<String> list = new ArrayList<>();
+                list.add(email);
+                idToEmails.put(id, list);
+            }
+        }
+        for (int id : idToEmails.keySet()) {
+            ArrayList<String> emails = idToEmails.get(id);
+            Collections.sort(emails);
+            emails.add(0, names[id]);
+            result.add(emails);
+        }
+        return result;
+    }
+}
