c92b867ac6311ca207807dafb9bb35fbbdd99f9f
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-12 23:16:52 -0500

    Time: 150 ms (9.79%) | Memory: 45.8 MB (74.33%) - LeetSync

---
+++ Grind-75/416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean canPartition(int[] nums) {
+        int sum = 0;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+
+        if (sum % 2 == 1) {
+            return false;
+        }
+
+        int target = sum / 2;
+        HashSet<Integer> set = new HashSet<>();
+        set.add(0);
+        for (int i = 0; i < nums.length; i++) {
+            HashSet<Integer> newSet = new HashSet<>(set);
+            for (int n : set) {
+                if (n + nums[i] == target) return true;
+                newSet.add(n + nums[i]);
+            }
+            set = newSet;
+        }
+        return false;
+    }
+}
