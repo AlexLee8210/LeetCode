01a848c8c5ab8014bccc3b70a13317abdeb6bd29
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-13 14:24:34 -0500

    Time: 1 ms (66.36%) | Memory: 42.2 MB (23.33%) - LeetSync

---
+++ Grind-75/17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        if (digits.length() == 0) return new ArrayList<>();
+        HashMap<Character, char[]> map = new HashMap<>();
+        map.put('2', new char[]{'a', 'b', 'c'});
+        map.put('3', new char[]{'d', 'e', 'f'});
+        map.put('4', new char[]{'g', 'h', 'i'});
+        map.put('5', new char[]{'j', 'k', 'l'});
+        map.put('6', new char[]{'m', 'n', 'o'});
+        map.put('7', new char[]{'p', 'q', 'r', 's'});
+        map.put('8', new char[]{'t', 'u', 'v'});
+        map.put('9', new char[]{'w', 'x', 'y', 'z'});
+        
+        ArrayList<String> res = new ArrayList<>();
+        helper(digits, map, 0, res, new StringBuilder());
+        return res;
+    }
+
+    private void helper(String digits, HashMap<Character, char[]> map, int index, ArrayList<String> res, StringBuilder sb) {
+        if (index == digits.length()) {
+            res.add(sb.toString());
+            return;
+        }
+        for (char c : map.get(digits.charAt(index))) {
+            sb.append(c);
+            helper(digits, map, index + 1, res, sb);
+            sb.delete(sb.length() - 1, sb.length());
+        }
+    }
+}
