456dc5595d4cf71c24eeb4c6bad51f4c4a47e6e2
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-13 10:29:48 -0500

    Time: 1 ms (100.00%) | Memory: 42.7 MB (16.94%) - LeetSync

--- Grind-75/8-string-to-integer-atoi/string-to-integer-atoi.java
+++ Grind-75/8-string-to-integer-atoi/string-to-integer-atoi.java
@@ -1,65 +1,22 @@
-// class Solution {
-//     public int myAtoi(String s) {
-//         int num = 0;
-//         boolean isPos = true;
-//         boolean readingZero = true;
-//         boolean readingNum = false;
-//         for (char c : s.toCharArray()) {
-//             if ((readingNum && readingZero) && !Character.isDigit(c)) break;
-//             if (c == ' ') continue;
-//             if (c == '-') {
-//                 isPos = false;
-//             } else if (Character.isDigit(c)) {
-//                 readingNum = true;
-//                 if (c == '0' && readingZero) {
-//                     readingZero = true;
-//                     continue;
-//                 }
-//                 num *= 10;
-//                 num += c - '0';
-//             }
-//         }
-//         return isPos ? num : -num;
-//     }
-// }
 class Solution {
     public int myAtoi(String s) {
-        int num = 0;
+        char[] chars = s.toCharArray();
+        int i = 0;
         boolean isPos = true;
-        boolean reading = true;
-        boolean readingZero = false;
-        for (char c : s.toCharArray()) {
-            if (!reading && !Character.isDigit(c)) break;
-            if (c == ' ') continue;
-            if (c == '-' || c == '+') {
-                isPos = c == '+';
-                reading = false;
-            } else if (Character.isDigit(c)) {
-                reading = false;
-                if (c == '0' && readingZero) {
-                    readingZero = true;
-                    continue;
-                } else {
-                    readingZero = false;
-                }
-
-                if (num > Integer.MAX_VALUE / 10) {
-                    return isPos ? Integer.MAX_VALUE : Integer.MIN_VALUE;
-                } else if (num == Integer.MAX_VALUE / 10) {
-                    if (isPos) {
-                        if (c >= '7') {
-                            return Integer.MAX_VALUE;
-                        }
-                    } else if (c >= '8') {
-                        return Integer.MIN_VALUE;
-                    }
-                }
-                num *= 10;
-                num += c - '0';
-            } else {
-                if (reading && (c != ' ' || c != '-' || c != '+')) break;
-                reading = false;
+        int num = 0;
+        while (i < chars.length && chars[i] == ' ') i++;
+        if (i < chars.length && (chars[i] == '-' || chars[i] == '+')) {
+            isPos = chars[i++] == '+';
+        }
+        while (i < chars.length && Character.isDigit(chars[i])) {
+            if (num > Integer.MAX_VALUE / 10
+                || (num == Integer.MAX_VALUE / 10 && chars[i] >= (isPos ? '7' : '8')))
+            {
+                num = isPos ? Integer.MAX_VALUE : Integer.MIN_VALUE;
+                return num;
             }
+            num = num * 10 + (chars[i] - '0');
+            i++;
         }
         return isPos ? num : -num;
     }
