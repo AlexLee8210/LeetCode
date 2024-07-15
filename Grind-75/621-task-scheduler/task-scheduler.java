7f1d526331d5a9ad3888acdc596474b07ea1e57e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-15 09:43:44 -0500

    Time: 4 ms (69.04%) | Memory: 46 MB (14.58%) - LeetSync

--- Grind-75/621-task-scheduler/task-scheduler.java
+++ Grind-75/621-task-scheduler/task-scheduler.java
@@ -1,33 +1,20 @@
 class Solution {
     public int leastInterval(char[] tasks, int n) {
-     Map<Character, Integer> map = new HashMap<>();
-    for (int i = 0; i < tasks.length; i++) {
-        map.put(tasks[i], map.getOrDefault(tasks[i], 0) + 1); // map key is TaskName, and value is number of times to be executed.
-    }
-    PriorityQueue<Map.Entry<Character, Integer>> q = new PriorityQueue<>( //frequency sort
-            (a,b) -> a.getValue() != b.getValue() ? b.getValue() - a.getValue() : a.getKey() - b.getKey());
-
-    q.addAll(map.entrySet());
-
-    int count = 0;
-    while (!q.isEmpty()) {
-        int k = n + 1;
-        List<Map.Entry> tempList = new ArrayList<>();
-        while (k > 0 && !q.isEmpty()) {
-            Map.Entry<Character, Integer> top = q.poll(); // most frequency task
-            top.setValue(top.getValue() - 1); // decrease frequency, meaning it got executed
-            tempList.add(top); // collect task to add back to queue
-            k--;
-            count++; //successfully executed task
+        int[] cnts = new int[26];
+        int max = 0;
+        int numMax = 0;
+        for (char task : tasks) {
+            cnts[task - 'A']++;
+            if (max < cnts[task - 'A']) {
+                max = cnts[task - 'A'];
+                numMax = 1;
+            } else if (max == cnts[task - 'A']) {
+                numMax++;
+            }
         }
-
-        for (Map.Entry<Character, Integer> e : tempList) {
-            if (e.getValue() > 0) q.add(e); // add valid tasks 
-        }
-
-        if (q.isEmpty()) break;
-        count = count + k; // if k > 0, then it means we need to be idle
-    }
-    return count;
+        int numSpaces = max - 1;
+        int spaceCount = numSpaces * (n - numMax + 1);
+        int numIdles = Math.max(0, spaceCount - tasks.length + (numMax * max));
+        return numIdles + tasks.length;
     }
 }
