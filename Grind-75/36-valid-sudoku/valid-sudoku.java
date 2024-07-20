042e0010c1a0c0e1c54871d532382ab02cc852cb
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-20 18:00:53 -0500

    Time: 2 ms (81.18%) | Memory: 44.6 MB (41.70%) - LeetSync

--- Grind-75/36-valid-sudoku/valid-sudoku.java
+++ Grind-75/36-valid-sudoku/valid-sudoku.java
@@ -18,13 +18,6 @@ class Solution {
                 if (board[rIndex][cIndex] != '.' && !grid.add(board[rIndex][cIndex])) {
                     return false;
                 }
-                /*
-                r: 0 0 0 0 0 0 0 0 0 1 1 1 1 1 1 1 1 1
-                a: 0 0 0 1 1 1 2 2 2 0 0 0 1 1 1 2 2 2
-
-                c: 0 1 2 3 4 5 6 7 8 0 1 2 3 4 5 6 7 8
-                b: 0 1 2 0 1 2 0 1 2 0 1 2 0 1 2 0 1 2
-                */
             }
         }
         return true;
