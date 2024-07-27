787435bb9d08d000e27ac50978416f97c1135734
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-27 11:53:41 -0500

    Time: 0 ms (100.00%) | Memory: 43 MB (71.55%) - LeetSync

--- Grind-75/662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
+++ Grind-75/662-maximum-width-of-binary-tree/maximum-width-of-binary-tree.java
@@ -15,17 +15,18 @@
  */
 class Solution {
     public int widthOfBinaryTree(TreeNode root) {
-        return helper(root, 0, 1, new HashMap<>());
+        ArrayList<Integer> levels = new ArrayList<>();
+        return helper(root, 0, 1, levels);
     }
 
-    private int helper(TreeNode node, int level, int index, HashMap<Integer, Integer> map) {
+    private int helper(TreeNode node, int level, int index, ArrayList<Integer> levels) {
         if (node == null) return 0;
-        if (!map.containsKey(level)) {
-            map.put(level, index);
+        if (levels.size() <= level) {
+            levels.add(index);
         }
-        int cur = index - map.get(level) + 1;
-        int left = helper(node.left, level + 1, index * 2 - 1, map);
-        int right = helper(node.right, level + 1, index * 2, map);
+        int cur = index - levels.get(level) + 1;
+        int left = helper(node.left, level + 1, index * 2 - 1, levels);
+        int right = helper(node.right, level + 1, index * 2, levels);
         return Math.max(cur, Math.max(left, right));
     }
     
