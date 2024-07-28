148f2fc8b9b3c90cca7a1f47ec294a9b97de8036
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-28 15:50:00 -0500

    Time: 3 ms (28.61%) | Memory: 54.4 MB (97.12%) - LeetSync

---
+++ Grind-75/1538-maximum-points-you-can-obtain-from-cards/maximum-points-you-can-obtain-from-cards.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxScore(int[] cardPoints, int k) {
+        int sum = 0;
+        for (int i = cardPoints.length - k; i < cardPoints.length; i++) {
+            sum += cardPoints[i];
+        }
+        int max = sum;
+        int r = cardPoints.length - k;
+        int l = 0;
+        while (r < cardPoints.length) {
+            sum += cardPoints[l++];
+            sum -= cardPoints[r++];
+            max = Math.max(max, sum);
+        }
+        return max;
+    }
+}
