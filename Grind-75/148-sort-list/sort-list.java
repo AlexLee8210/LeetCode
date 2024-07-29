6a9b1304b44538566932265d3cf6abbbd15f5b25
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-07-28 22:52:31 -0500

    Time: 11 ms (40.84%) | Memory: 56.8 MB (43.61%) - LeetSync

--- Grind-75/148-sort-list/sort-list.java
+++ Grind-75/148-sort-list/sort-list.java
@@ -10,21 +10,42 @@
  */
 class Solution {
     public ListNode sortList(ListNode head) {
-        PriorityQueue<ListNode> pq = new PriorityQueue<>((a, b) -> a.val - b.val);
-        ListNode node = head;
-        while (node != null) {
-            pq.offer(node);
+        if (head == null || head.next == null) return head;
+
+        ListNode slow = head, fast = head;
+        ListNode prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        prev.next = null;
+
+        ListNode n1 = sortList(head);
+        ListNode n2 = sortList(slow);
+
+        return merge(n1, n2);
+    }
+
+    private ListNode merge(ListNode n1, ListNode n2) {
+        ListNode node = new ListNode();
+        ListNode header = node;
+        while (n1 != null || n2 != null) {
+            if (n1 == null) {
+                node.next = n2;
+                n2 = n2.next;
+            } else if (n2 == null) {
+                node.next = n1;
+                n1 = n1.next;
+            } else if (n2.val < n1.val) {
+                node.next = n2;
+                n2 = n2.next;
+            } else {
+                node.next = n1;
+                n1 = n1.next;
+            }
             node = node.next;
         }
-        head = pq.poll();
-        node = head;
-        while (!pq.isEmpty()) {
-            node.next = pq.poll();
-            node = node.next;
-        }
-        if (node != null) {
-            node.next = null;
-        }
-        return head;
+        return header.next;
     }
 }
