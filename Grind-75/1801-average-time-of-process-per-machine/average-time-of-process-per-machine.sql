d4b28414867a1af4d1377c2379bb89e6e259b9ae
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-04 10:56:08 -0500

    Time: 239 ms (26.19%) | Memory: 0B (100.00%) - LeetSync

--- Grind-75/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
+++ Grind-75/1801-average-time-of-process-per-machine/average-time-of-process-per-machine.sql
@@ -1,8 +1,7 @@
 # Write your MySQL query statement below
-SELECT machine_id, ROUND(MAX(avg) - MIN(avg), 3) as processing_time
-FROM (
-    SELECT *, AVG(timestamp) as avg
-    FROM Activity
-    GROUP BY machine_id, activity_type
-) AS t1
+SELECT a.machine_id, ROUND(
+    (SELECT AVG(a1.timestamp) FROM Activity a1 WHERE a1.activity_type = 'end' AND a1.machine_id = a.machine_id) -
+    (SELECT AVG(a1.timestamp) FROM Activity a1 WHERE a1.activity_type = 'start' AND a1.machine_id = a.machine_id), 3
+) as processing_time
+FROM Activity a
 GROUP BY machine_id;
