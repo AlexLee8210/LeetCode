273fa65cc0ecc564d3fcb95416110ce13fc5ac0c
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-04 11:42:46 -0500

    Time: 787 ms (78.22%) | Memory: 0B (100.00%) - LeetSync

--- SQL-50/1415-students-and-examinations/students-and-examinations.sql
+++ SQL-50/1415-students-and-examinations/students-and-examinations.sql
@@ -6,10 +6,3 @@ LEFT JOIN Examinations e
 ON e.student_id = s.student_id and sub.subject_name = e.subject_name
 GROUP BY s.student_id, sub.subject_name
 ORDER BY s.student_id;
-
-
-    -- SELECT e.student_id, sub.subject_name, COUNT(sub.subject_name)
-    -- FROM Examinations e
-    -- CROSS JOIN Subjects sub
-    -- -- ON sub.subject_name = e.subject_name
-    -- GROUP BY e.student_id, sub.subject_name
