d71eb7c2db2d1d73bbe42ce240160c74f3500500
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-04 12:00:08 -0500

    Time: 642 ms (8.01%) | Memory: 0B (100.00%) - LeetSync

--- SQL-50/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
+++ SQL-50/570-managers-with-at-least-5-direct-reports/managers-with-at-least-5-direct-reports.sql
@@ -1,10 +1,7 @@
 # Write your MySQL query statement below
 SELECT e1.name
 FROM Employee e1
-RIGHT JOIN (
-    SELECT managerId, COUNT(managerId) as employeeCount
-    FROM Employee
-    WHERE managerId IN (SELECT id FROM Employee)
-    GROUP BY managerId
-) e2 ON e2.managerId = e1.id
-WHERE e2.employeeCount >= 5;
+INNER JOIN Employee e2
+ON e2.managerId = e1.id
+GROUP BY e1.id
+HAVING COUNT(e2.managerId) >= 5;
