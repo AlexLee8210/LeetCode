139497bf64218c5ec2825a3f22712d05bd9f8063
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-11 22:00:43 -0500

    Time: 15 ms (96.66%) | Memory: 44.1 MB (95.79%) - LeetSync

--- Grind-75/1142-minimum-knight-moves/minimum-knight-moves.java
+++ Grind-75/1142-minimum-knight-moves/minimum-knight-moves.java
@@ -1,29 +1,26 @@
 class Solution {
-    private int[][] dirs = new int[][]{{1, 2}, {2, 1}, {-1, 2}, {2, -1}, {-1, -2}, {-2, -1}, {1, -2}, {-2, 1}};
+    private int[][] dirs = new int[][]{{-1, -2}, {-2, -1}};
     public int minKnightMoves(int x, int y) {
-        if (x == 0 && y == 0) return 0;
-        Queue<int[]> q = new LinkedList<>();
-        boolean[][] vis = new boolean[601][601];
-        q.offer(new int[]{0, 0});
+        HashMap<String, Integer> memo = new HashMap<>();
+        return dfs(Math.abs(x), Math.abs(y), memo);
+    }
 
-        int step = 0;
-        while (!q.isEmpty()) {
-            step++;
-            int size = q.size();
-            for (int i = 0; i < size; i++) {
-                int[] pos = q.poll();
-                if (vis[pos[0] + 300][pos[1] + 300]) continue;
-                vis[pos[0] + 300][pos[1] + 300] = true;
-                for (int[] dir : dirs) {
-                    int newX = pos[0] + dir[0];
-                    int newY = pos[1] + dir[1];
-
-                    if (newX == x && newY == y) return step;
-                    q.offer(new int[]{newX, newY});
-                }
-            }
+    private int dfs(int x, int y, HashMap<String, Integer> memo) {
+        String key = x + "," + y;
+        if (memo.containsKey(key)) {
+            return memo.get(key);
         }
 
-        return step;
+        if (x == 0 && y == 0) {
+            return 0;
+        } else if (x + y == 2) {
+            return 2;
+        }
+        int res = Math.min(
+            dfs(Math.abs(x - 1), Math.abs(y - 2), memo),
+            dfs(Math.abs(x - 2), Math.abs(y - 1), memo)
+        ) + 1;
+        memo.put(key, res);
+        return res;
     }
 }
