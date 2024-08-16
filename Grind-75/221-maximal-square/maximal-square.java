e822392431a3177a5815f86d7e28cb0ec2044ad6
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-16 18:42:37 -0500

    Time: 30 ms (11.19%) | Memory: 57.9 MB (8.23%) - LeetSync

---
+++ Grind-75/221-maximal-square/maximal-square.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int maximalSquare(char[][] matrix) {
+        int m = matrix.length, n = matrix[0].length;
+        int maxLen = 0;
+        int[][] dp = new int[m][n];
+
+        for (int r = 0; r < m; r++) {
+            for (int c = 0; c < n; c++) {
+                if (matrix[r][c] == '0') continue;
+
+                dp[r][c] = 1;
+                if (r != 0 && c != 0) {
+                    dp[r][c] = Math.min(dp[r - 1][c - 1], Math.min(dp[r - 1][c], dp[r][c - 1])) + 1;
+                }
+                maxLen = Math.max(maxLen, dp[r][c]);
+            }
+        }
+        print(dp);
+        return maxLen * maxLen;
+    }
+
+    private void print(int[][] mat) {
+        for (int r = 0; r < mat.length; r++) {
+            System.out.println(Arrays.toString(mat[r]));
+        }
+    }
+}
