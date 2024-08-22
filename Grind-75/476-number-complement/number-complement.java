e8aeb45f8216e6e1a4c78805432876784ab93de3
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-22 00:29:03 -0500

    Time: 0 ms (100.00%) | Memory: 40.4 MB (37.76%) - LeetSync

--- Grind-75/476-number-complement/number-complement.java
+++ Grind-75/476-number-complement/number-complement.java
@@ -3,6 +3,6 @@ class Solution {
         if (num == 0) return 1;
 
         int exp = (int) (Math.log(num) / Math.log(2) + 1);
-        return (int)((long)Math.pow(2, exp) - 1) ^ num;
+        return ((1 << exp) - 1) ^ num;
     }
 }
