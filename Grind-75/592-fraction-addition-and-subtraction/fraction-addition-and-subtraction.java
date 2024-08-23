c0bd1892697023a54454a0ca064540c5b2e50408
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-23 02:09:59 -0500

    Time: 5 ms (75.23%) | Memory: 41.9 MB (61.01%) - LeetSync

---
+++ Grind-75/592-fraction-addition-and-subtraction/fraction-addition-and-subtraction.java
@@ -0,0 +1,57 @@
+class Solution {
+    public String fractionAddition(String expression) {
+        Stack<Integer> numerators = new Stack<>();
+        Stack<Integer> denominators = new Stack<>();
+        int cur = 0;
+        boolean isPositive = true;
+        char[] chars = expression.toCharArray();
+        for (int i = 0; i < chars.length; i++) {
+            char c = chars[i];
+            if (Character.isDigit(c)) {
+                cur *= 10;
+                cur += (int)(c - '0');
+            } else if (c == '/') {
+                numerators.push(cur * (isPositive ? 1 : -1));
+                isPositive = true;
+                cur = 0;
+            } else { // c == '-' or '+'
+                if (i - 1 >= 0 && chars[i - 1] != '/') {
+                    denominators.push(cur * (isPositive ? 1 : -1));
+                    cur = 0;
+                }
+                isPositive = c == '+';
+            }
+        }
+        denominators.push(cur * (isPositive ? 1 : -1));
+
+
+        while (numerators.size() > 1) {
+            int n2 = numerators.pop();
+            int n1 = numerators.pop();
+            int d2 = denominators.pop();
+            int d1 = denominators.pop();
+            int gcd = gcd(d1, d2);
+            n2 *= d1 / gcd;
+            n1 *= d2 / gcd;
+            numerators.push(n1 + n2);
+            denominators.push(d1 / gcd * d2);
+        }
+        int numerator = numerators.pop();
+        int denominator = denominators.pop();
+        int gcd = gcd(numerator, denominator);
+        numerator /= gcd;
+        denominator /= gcd;
+        if (denominator < 0) {
+            denominator *= -1;
+            numerator *= -1;
+        }
+        return numerator + "/" + denominator;
+    }
+
+    private int gcd(int a, int b) {
+        if (a == 0) {
+            return b;
+        }
+        return gcd(b % a, a);
+    }
+}
