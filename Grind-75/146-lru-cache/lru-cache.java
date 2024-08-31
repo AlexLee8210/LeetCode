2f0981fd3c2a680df5924c366bb52fe0395b7620
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-08-31 12:29:49 -0500

    Time: 44 ms (76.43%) | Memory: 112.5 MB (81.34%) - LeetSync

--- Grind-75/146-lru-cache/lru-cache.java
+++ Grind-75/146-lru-cache/lru-cache.java
@@ -1,34 +1,36 @@
 class LRUCache {
+
     private class Node {
         int key;
-        int val;
-        Node prev, next;
-        public Node(int k, int v) {
-            key = k;
-            val = v;
+        int value;
+        Node next, prev;
+
+        public Node(int key, int value) {
+            this.key = key;
+            this.value = value;
         }
     }
 
-    HashMap<Integer, Node> cache;
-    int cap;
-    Node head, tail;
+    private int cap;
+    private HashMap<Integer, Node> map;
+
+    private Node head, tail;
+
+    private void add(Node n) {
+        n.prev = tail.prev;
+        n.next = tail;
+        tail.prev.next = n;
+        tail.prev = n;
+    }
 
     private void remove(Node n) {
         n.prev.next = n.next;
         n.next.prev = n.prev;
     }
 
-    private void add(Node n) {
-        Node lastNode = tail.prev;
-        lastNode.next = n;
-        n.prev = lastNode;
-        n.next = tail;
-        tail.prev = n;
-    }
-
     public LRUCache(int capacity) {
+        map = new HashMap<>();
         cap = capacity;
-        cache = new HashMap<>(cap + 1);
         head = new Node(0, 0);
         tail = new Node(0, 0);
         head.next = tail;
@@ -36,49 +38,31 @@ class LRUCache {
     }
     
     public int get(int key) {
-        if (!cache.containsKey(key)) return -1;
-        Node n = cache.get(key);
-        remove(n);
-        add(n);
+        if (!map.containsKey(key)) {
+            return -1;
+        }
 
-        return n.val;
+        Node node = map.get(key);
+        remove(node);
+        add(node);
+        return node.value;
     }
     
     public void put(int key, int value) {
-        if (cache.containsKey(key)) {
-            remove(cache.get(key));
-        }
-        Node n = new Node(key, value);
-        cache.put(key, n);
-        add(n);
-        if (cache.size() > cap) {
-            Node lru = head.next;
-            remove(lru);
-            cache.remove(lru.key);
+        if (map.containsKey(key)) {
+            Node n = map.get(key);
+            remove(n);
+            n.value = value;
+            add(n);
+        } else {
+            if (map.size() == cap) {
+                Node n = head.next;
+                remove(n);
+                map.remove(n.key);
+            }
+            Node n = new Node(key, value);
+            add(n);
+            map.put(key, n);
         }
     }
-
-    // private void print() {
-    //     if (head.next == tail) {
-    //         System.out.println("[]");
-    //         return;
-    //     }
-    //     Node cur = head.next;
-    //     StringBuilder sb = new StringBuilder();
-    //     sb.append("[");
-    //     while (cur != tail) {
-    //         sb.append(cur.key).append(", ");
-    //         cur = cur.next;
-    //     }
-    //     sb.delete(sb.length() - 2, sb.length());
-    //     sb.append("]");
-    //     System.out.println(sb);
-    // }
 }
-
-/**
- * Your LRUCache object will be instantiated and called as such:
- * LRUCache obj = new LRUCache(capacity);
- * int param_1 = obj.get(key);
- * obj.put(key,value);
- */
