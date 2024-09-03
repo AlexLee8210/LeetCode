6d5dc9c2885389f867c244f2ec8028d77fa8c3b6
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-09-03 10:49:37 -0500

    Time: 19 ms (11.69%) | Memory: 45.2 MB (87.17%) - LeetSync

---
+++ Grind-75/271-encode-and-decode-strings/encode-and-decode-strings.java
@@ -0,0 +1,39 @@
+public class Codec {
+
+    // Encodes a list of strings to a single string.
+    public String encode(List<String> strs) {
+        StringBuilder sb = new StringBuilder();
+        for (String k : strs) {
+            sb.append(k.length() + "_" + k);
+        }
+        return sb.toString();
+    }
+
+    // Decodes a single string to a list of strings.
+    public List<String> decode(String s) {
+        List<String> res = new ArrayList<>();
+        int n = 0;
+        int i = 0;
+        char[] chars = s.toCharArray();
+
+        while (i < chars.length) {
+            while (i < chars.length && chars[i] != '_' && Character.isDigit(chars[i])) {
+                n = n * 10 + (chars[i] - '0');
+                i++;
+            }
+            i++;
+            StringBuilder sb = new StringBuilder("");
+            for (int j = 0; j < n; j++) {
+                sb.append(chars[j + i]);
+            }
+            res.add(sb.toString());
+            i += n;
+            n = 0;
+        }
+        return res;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.decode(codec.encode(strs));
