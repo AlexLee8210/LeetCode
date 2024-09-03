9a660cf4cf3d1dc466db8d08e410c978040716ee
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-09-03 09:58:42 -0500

    Time: 0 ms (100.00%) | Memory: 41.7 MB (5.75%) - LeetSync

--- Grind-75/232-implement-queue-using-stacks/implement-queue-using-stacks.java
+++ Grind-75/232-implement-queue-using-stacks/implement-queue-using-stacks.java
@@ -11,21 +11,26 @@ class MyQueue {
     public void push(int x) {
         stack1.push(x);
     }
-    
+    // 1 2 3
+    // 3 2 1
     public int pop() {
-        if (stack2.isEmpty()) {
-            while (!stack1.isEmpty()) {
-                stack2.push(stack1.pop());
-            }
+        if (!stack2.isEmpty()) {
+            return stack2.pop();
+        }
+
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
         }
         return stack2.pop();
     }
     
     public int peek() {
-        if (stack2.isEmpty()) {
-            while (!stack1.isEmpty()) {
-                stack2.push(stack1.pop());
-            }
+        if (!stack2.isEmpty()) {
+            return stack2.peek();
+        }
+
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
         }
         return stack2.peek();
     }
