fe375970dbf585e67d82beeca49d4e1dab121dba
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-09-05 10:26:41 -0500

    Time: 63 ms (80.00%) | Memory: 55.3 MB (68.24%) - LeetSync

---
+++ Grind-75/288-unique-word-abbreviation/unique-word-abbreviation.java
@@ -0,0 +1,34 @@
+class ValidWordAbbr {
+    HashMap<String, HashSet<String>> map;
+
+    public ValidWordAbbr(String[] dictionary) {
+        map = new HashMap<>();
+        for (String k : dictionary) {
+            String ab = abbr(k);
+            if (!map.containsKey(ab)) {
+                map.put(ab, new HashSet<>());
+            }
+            map.get(ab).add(k);
+        }
+    }
+
+    private String abbr(String k) {
+        if (k.length() <= 2) return k;
+        StringBuilder sb = new StringBuilder();
+        sb.append(k.charAt(0)).append(k.length() - 2).append(k.charAt(k.length() - 1));
+        return sb.toString();
+    }
+    
+    public boolean isUnique(String word) {
+        String ab = abbr(word);
+        if (!map.containsKey(ab)) return true;
+        HashSet<String> set = map.get(ab);
+        return set.size() == 1 && set.contains(word);
+    }
+}
+
+/**
+ * Your ValidWordAbbr object will be instantiated and called as such:
+ * ValidWordAbbr obj = new ValidWordAbbr(dictionary);
+ * boolean param_1 = obj.isUnique(word);
+ */
