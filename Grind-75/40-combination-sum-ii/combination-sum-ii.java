784c19c74e097b93f774dd88fd0b5fc2070f3ea0
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-10-07 12:30:34 -0500

    Time: 4 ms (42.58%) | Memory: 43.4 MB (17.56%) - LeetSync

---
+++ Grind-75/40-combination-sum-ii/combination-sum-ii.java
@@ -0,0 +1,26 @@
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(candidates);
+        helper(candidates, 0, target, new ArrayList<>(), res);
+        return res;
+    }
+
+    private void helper(int[] nums, int index, int target, List<Integer> cur, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(cur));
+            return;
+        }
+
+        if (index == nums.length || target < 0) {
+            return;
+        }
+
+        for (int i = index; i < nums.length; ++i) {
+            if (i > index && nums[i] == nums[i - 1]) continue;
+            cur.add(nums[i]);
+            helper(nums, i + 1, target - nums[i], cur, res);
+            cur.remove(cur.size() - 1);
+        }
+    }
+}
