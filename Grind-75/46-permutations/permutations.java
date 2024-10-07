b07f02fe0d666c5475fd5809dea705d70c0adaf6
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-10-06 22:58:04 -0500

    Time: 2 ms (45.39%) | Memory: 44.4 MB (68.33%) - LeetSync

--- Grind-75/46-permutations/permutations.java
+++ Grind-75/46-permutations/permutations.java
@@ -1,22 +1,20 @@
 class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        helper(nums, new HashSet<>(), new ArrayList<>(), res);
+        helper(nums, new ArrayList<>(), res);
         return res;
     }
 
-    private void helper(int[] nums, Set<Integer> vis, List<Integer> cur, List<List<Integer>> res) {
-        if (vis.size() == nums.length) {
+    private void helper(int[] nums, List<Integer> cur, List<List<Integer>> res) {
+        if (cur.size() == nums.length) {
             res.add(new ArrayList<>(cur));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            if (vis.contains(nums[i])) continue;
+            if (cur.contains(nums[i])) continue;
             cur.add(nums[i]);
-            vis.add(nums[i]);
-            helper(nums, vis, cur, res);
-            vis.remove(nums[i]);
+            helper(nums, cur, res);
             cur.remove(cur.size() - 1);
         }
     }
