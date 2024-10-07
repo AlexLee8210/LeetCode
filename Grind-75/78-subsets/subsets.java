1bccab831c62adf116e92bd739c447f182171af1
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-10-06 23:22:56 -0500

    Time: 1 ms (54.68%) | Memory: 42.2 MB (96.62%) - LeetSync

--- Grind-75/78-subsets/subsets.java
+++ Grind-75/78-subsets/subsets.java
@@ -1,18 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> res = new ArrayList<>();
-        helper(nums, 0, new ArrayList<Integer>(), res);
+        helper(nums, 0, new ArrayList<>(), res);
         return res;
     }
-    private void helper(int[] nums, int i, List<Integer> cur, List<List<Integer>> res) {
-        if (i == nums.length) {
-            res.add(new ArrayList<>(cur));
-            return;
-        }
 
-        helper(nums, i + 1, cur, res);
-        cur.add(nums[i]);
-        helper(nums, i + 1, cur, res);
-        cur.removeLast();
+    private void helper(int[] nums, int index, List<Integer> cur, List<List<Integer>> res) {
+        res.add(new ArrayList<>(cur));
+
+        for (int i = index; i < nums.length; ++i) {
+            cur.add(nums[i]);
+            helper(nums, i + 1, cur, res);
+            cur.remove(cur.size() - 1);
+        }
     }
 }
