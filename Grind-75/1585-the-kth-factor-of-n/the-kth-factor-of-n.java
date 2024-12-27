a84de1ba3bda89a6dce2636a35ef3a8b1c40a705
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-27 17:33:51 -0600

    Time: 1 ms (40.61%) | Memory: 41 MB (10.40%) - LeetSync

---
+++ Grind-75/1585-the-kth-factor-of-n/the-kth-factor-of-n.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int kthFactor(int n, int k) {
+        int top = (int) Math.floor(Math.sqrt(n));
+        ArrayList<Integer> factors = new ArrayList<Integer>(Arrays.asList(1));
+        // int factor = 1;
+        // int index = 1;
+
+        for (int i = 2; i <= top; ++i) {
+            if (n % i == 0) {
+                factors.add(i);
+            }
+        }
+
+        if (k <= factors.size()) {
+            return factors.get(k - 1);
+        }
+
+        int last = factors.getLast();
+        boolean sqrt = last * last == n;
+
+        int size = factors.size() * 2 + (sqrt ? -1 : 0);
+        if (k > size) {
+            return -1;
+        }
+
+        int cofactorIndex = factors.size() - (k - factors.size()) + (sqrt ? -1 : 0);
+        return n / factors.get(cofactorIndex);
+    }
+}
