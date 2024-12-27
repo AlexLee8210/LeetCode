fb4af4956953feb85465f90ba46471dea5c08ae2
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-27 17:39:53 -0600

    Time: 31 ms (60.82%) | Memory: 47 MB (6.79%) - LeetSync

---
+++ Grind-75/2487-optimal-partition-of-string/optimal-partition-of-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int partitionString(String s) {
+        HashSet<Character> set = new HashSet<>();
+        int num = 1;
+        for (char c : s.toCharArray()) {
+            if (set.contains(c)) {
+                ++num;
+                set = new HashSet<>();
+            }
+            set.add(c);
+        }
+        return num;
+    }
+}
