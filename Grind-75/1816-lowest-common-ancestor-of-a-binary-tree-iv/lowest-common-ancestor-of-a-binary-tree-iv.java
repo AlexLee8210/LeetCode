045035862a487824a70578516720c748943faa28
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-27 19:04:53 -0600

    Time: 3 ms (72.76%) | Memory: 45.6 MB (68.37%) - LeetSync

---
+++ Grind-75/1816-lowest-common-ancestor-of-a-binary-tree-iv/lowest-common-ancestor-of-a-binary-tree-iv.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+
+    HashMap<TreeNode, Boolean> containsNode = new HashMap<>();
+
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode[] nodes) {
+        Set<TreeNode> set = new HashSet<>();
+        for (TreeNode node : nodes) {
+            set.add(node);
+        }
+
+        return lca(root, set);
+    }
+
+    private TreeNode lca(TreeNode root, Set<TreeNode> nodes) {
+        if (root == null || nodes.contains(root)) return root;
+
+        TreeNode left = lca(root.left, nodes);
+        TreeNode right = lca(root.right, nodes);
+
+        if (left != null && right != null) return root;
+        else if (left != null) return left;
+        else return right;
+    }
+}
