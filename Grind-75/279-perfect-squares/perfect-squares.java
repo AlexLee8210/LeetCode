152b087e4c85a93a349228dc1b8a8352fac43c59
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-28 08:00:44 -0600

    Time: 28 ms (77.12%) | Memory: 42.5 MB (80.63%) - LeetSync

--- Grind-75/279-perfect-squares/perfect-squares.java
+++ Grind-75/279-perfect-squares/perfect-squares.java
@@ -2,26 +2,18 @@ class Solution {
     public int numSquares(int n) {
         if (n <= 3) return n;
 
-        int[] num = new int[n + 1];
-        num[1] = 1;
-        num[2] = 2;
-        num[3] = 3;
+        int[] dp = new int[n + 1];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 3;
 
-        List<Integer> sqrts = new ArrayList<>();
-
-        for (int i = 2; i <= n; ++i) {
-            int sqrt = (int) Math.sqrt(i);
-            if (sqrt * sqrt == i) {
-                num[i] = 1;
-                sqrts.add(i);
-            } else {
-                num[i] = num[i - 1] + 1;
-                for (int k : sqrts) {
-                    num[i] = Math.min(num[i], num[i - k] + 1);
-                }
+        for (int i = 4; i <= n; ++i) {
+            dp[i] = dp[i - 1] + 1;
+            for (int j = 2; j * j <= i; ++j) {
+                dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
             }
         }
 
-        return num[n];
+        return dp[n];
     }
 }
