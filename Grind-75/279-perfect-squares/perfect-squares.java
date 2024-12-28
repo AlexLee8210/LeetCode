b96620a5352caa1f7a475f0e8d677c8adba0469e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-28 08:00:59 -0600

    Time: 28 ms (77.12%) | Memory: 42.3 MB (90.43%) - LeetSync

--- Grind-75/279-perfect-squares/perfect-squares.java
+++ Grind-75/279-perfect-squares/perfect-squares.java
@@ -1,13 +1,8 @@
 class Solution {
     public int numSquares(int n) {
-        if (n <= 3) return n;
-
         int[] dp = new int[n + 1];
-        dp[1] = 1;
-        dp[2] = 2;
-        dp[3] = 3;
 
-        for (int i = 4; i <= n; ++i) {
+        for (int i = 1; i <= n; ++i) {
             dp[i] = dp[i - 1] + 1;
             for (int j = 2; j * j <= i; ++j) {
                 dp[i] = Math.min(dp[i], dp[i - j * j] + 1);
