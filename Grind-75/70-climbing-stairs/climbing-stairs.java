3074fdfc3919aba06a29104dd23897b430c6587e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-28 07:25:45 -0600

    Time: 0 ms (100.00%) | Memory: 40.4 MB (54.34%) - LeetSync

--- Grind-75/70-climbing-stairs/climbing-stairs.java
+++ Grind-75/70-climbing-stairs/climbing-stairs.java
@@ -1,27 +1,16 @@
 class Solution {
     public int climbStairs(int n) {
-        // int pprev = 0;
-        // int prev = 0;
-        // int cur = 0;
-
-        // for (int i = 1; i <= n; ++i) {
-        //     cur = prev + pprev + 1;
-        //     pprev = prev;
-        //     prev = cur;
-        // }
-
-        // return cur;
         if (n == 1) return 1;
-        int[] steps = new int[n + 1];
-        steps[0] = 1;
-        steps[1] = 1;
+        int pprev = 1;
+        int prev = 1;
+        int cur = 0;
 
         for (int i = 2; i <= n; ++i) {
-            steps[i] = steps[i - 1] + steps[i - 2];
+            cur = prev + pprev;
+            pprev = prev;
+            prev = cur;
         }
 
-        // System.out.println(Arrays.toString(steps));
-
-        return steps[n];
+        return cur;
     }
 }
