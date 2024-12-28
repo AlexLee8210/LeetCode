f2fc7568ea7cfebdaea6e5fd74353d976bbcb4d4
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-28 07:11:42 -0600

    Time: 454 ms (6.56%) | Memory: 60.6 MB (89.07%) - LeetSync

--- Grind-75/2555-design-sql/design-sql.java
+++ Grind-75/2555-design-sql/design-sql.java
@@ -1,43 +1,41 @@
 class SQL {
 
-    HashMap<String, HashMap<Integer, List<String>>> tables;
+    HashMap<String, List<List<String>>> tables;
     HashMap<String, Integer> lengths;
-    HashMap<String, Integer> ids;
 
     public SQL(List<String> names, List<Integer> columns) {
         tables = new HashMap<>();
         lengths = new HashMap<>();
-        ids = new HashMap<>();
 
         for (int i = 0; i < names.size(); ++i) {
-            tables.put(names.get(i), new HashMap<>());
+            tables.put(names.get(i), new ArrayList<>());
             lengths.put(names.get(i), columns.get(i));
-            ids.put(names.get(i), 0);
         }
     }
     
     public boolean ins(String name, List<String> row) {
         if (!tables.containsKey(name) || row.size() != lengths.get(name)) return false;
-        int id = ids.get(name) + 1;
-        HashMap<Integer, List<String>> table = tables.get(name);
-        table.put(id, row);
-        ids.put(name, id);
+        List<List<String>> table = tables.get(name);
+        table.add(row);
         return true;
     }
     
     public void rmv(String name, int rowId) {
         if (!tables.containsKey(name)) return;
-        HashMap<Integer, List<String>> table = tables.get(name);
+        List<List<String>> table = tables.get(name);
+        --rowId;
 
-        if (!table.containsKey(rowId)) return;
+        if (rowId < 0 || rowId >= table.size() || table.get(rowId) == null) return;
 
-        table.remove(rowId);
+        table.set(rowId, null);
     }
     
     public String sel(String name, int rowId, int columnId) {
         if (!tables.containsKey(name)) return "<null>";
-        HashMap<Integer, List<String>> table = tables.get(name);
-        if (!table.containsKey(rowId)) return "<null>";
+        List<List<String>> table = tables.get(name);
+        --rowId;
+
+        if (rowId < 0 || rowId >= table.size() || table.get(rowId) == null) return "<null>";
 
         List<String> row = table.get(rowId);
         if (columnId <= 0 || columnId > row.size()) return "<null>";
@@ -47,14 +45,16 @@ class SQL {
     
     public List<String> exp(String name) {
         if (!tables.containsKey(name)) return null;
-        HashMap<Integer, List<String>> table = tables.get(name);
+        List<List<String>> table = tables.get(name);
 
         List<String> res = new ArrayList<String>(table.size());
 
-        for (Integer id : table.keySet()) {
+        for (int id = 1; id <= table.size(); ++id) {
             StringBuilder sb = new StringBuilder();
             sb.append(id);
-            List<String> row = table.get(id);
+            List<String> row = table.get(id - 1);
+            if (row == null) continue;
+
             for (String s : row) {
                 sb.append(',').append(s);
             }
