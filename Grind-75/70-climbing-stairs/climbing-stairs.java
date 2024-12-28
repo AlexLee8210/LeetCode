d029534eb3b32cb88d511b81d70a96debc6a85ce
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-28 07:24:42 -0600

    Time: 0 ms (100.00%) | Memory: 40.4 MB (54.34%) - LeetSync

--- Grind-75/70-climbing-stairs/climbing-stairs.java
+++ Grind-75/70-climbing-stairs/climbing-stairs.java
@@ -1,20 +1,27 @@
 class Solution {
     public int climbStairs(int n) {
-        if (n == 0) {
-            return 0;
-        }
-        HashMap<Integer, Integer> memo = new HashMap<>();
-        memo.put(1, 1);
-        memo.put(2, 2);
-        return climbStairsHelper(n, memo);
-    }
+        // int pprev = 0;
+        // int prev = 0;
+        // int cur = 0;
 
-    private int climbStairsHelper(int n, HashMap<Integer, Integer> memo) {
-        if (memo.containsKey(n)) {
-            return memo.get(n);
+        // for (int i = 1; i <= n; ++i) {
+        //     cur = prev + pprev + 1;
+        //     pprev = prev;
+        //     prev = cur;
+        // }
+
+        // return cur;
+        if (n == 1) return 1;
+        int[] steps = new int[n + 1];
+        steps[0] = 1;
+        steps[1] = 1;
+
+        for (int i = 2; i <= n; ++i) {
+            steps[i] = steps[i - 1] + steps[i - 2];
         }
-        int ways = climbStairsHelper(n - 1, memo) + climbStairsHelper(n - 2, memo);
-        memo.put(n, ways);
-        return ways;
+
+        // System.out.println(Arrays.toString(steps));
+
+        return steps[n];
     }
 }
