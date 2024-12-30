9c5bf034010a1e147d8512f5d60d40cbc28a83ac
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-30 17:41:52 -0600

    Time: 4 ms (95.69%) | Memory: 64.6 MB (26.93%) - LeetSync

--- Grind-75/3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
+++ Grind-75/3420-find-occurrences-of-an-element-in-an-array/find-occurrences-of-an-element-in-an-array.java
@@ -1,18 +1,23 @@
 class Solution {
     public int[] occurrencesOfElement(int[] nums, int[] queries, int x) {
-        // int[] index = new int[100001];
-        // Arrays.fill(index, -1);
-        HashMap<Integer, Integer> index = new HashMap<>();
+        int max = Integer.MIN_VALUE;
+        for (int q : queries) {
+            max = Math.max(max, q);
+        }
+
+        int[] index = new int[max + 1];
+        Arrays.fill(index, -1);
         int occ = 0;
         for (int i = 0; i < nums.length; ++i) {
+            if (occ >= max) break;
             if (x == nums[i]) {
-                index.put(++occ, i);
+                index[++occ] = i;
             }
         }
 
         int[] res = new int[queries.length];
         for (int i = 0; i < queries.length; ++i) {
-            res[i] = index.getOrDefault(queries[i], -1);
+            res[i] = index[queries[i]];
         }
         return res;
     }
