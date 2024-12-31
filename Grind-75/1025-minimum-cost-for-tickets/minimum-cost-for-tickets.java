8b9106238812ed635f150a737863239545bb4a8e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2024-12-31 16:56:52 -0600

    Time: 2 ms (45.34%) | Memory: 41.1 MB (92.44%) - LeetSync

--- Grind-75/1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
+++ Grind-75/1025-minimum-cost-for-tickets/minimum-cost-for-tickets.java
@@ -2,42 +2,25 @@ class Solution {
     private int[] tickets = new int[]{1, 7, 30};
 
     public int mincostTickets(int[] days, int[] costs) {
-        // int max = days[days.length - 1];
-        // int[][] min = new int[max + 1][3];
+        int max = days[days.length - 1] + 1;
+        int[] dp = new int[max];
+        boolean[] travel = new boolean[max];
 
-        // for (int i = 0; )
-        int[] memo = new int[days[days.length - 1] + 1];
-        return helper(0, days, costs, memo);
-    }
-
-    private int helper(int index, int[] days, int[] costs, int[] memo) {
-        if (index >= days.length) return 0;
-        if (memo[index] != 0) return memo[index];
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i < costs.length; ++i) {
-            int nextIndex = bs(days, days[index] + tickets[i]);
-            int cost = helper(nextIndex, days, costs, memo) + costs[i];
-            min = Math.min(min, cost);
+        for (int day : days) {
+            travel[day] = true;
         }
 
-        memo[index] = min;
-        return min;
-    }
-
-    private int bs(int[] arr, int target) {
-        int l = 0, r = arr.length - 1;
-        if (target > arr[r]) return Integer.MAX_VALUE;
-
-        while (l < r) {
-            int m = l + (r - l) / 2;
-            if (arr[m] >= target) {
-                r = m;
-            } else {
-                l = m + 1;
+        for (int i = 1; i < max; ++i) {
+            if (!travel[i]) {
+                dp[i] = dp[i - 1];
+                continue;
+            }
+            dp[i] = Integer.MAX_VALUE;
+            for (int j = 0; j < costs.length; ++j) {
+                dp[i] = Math.min(dp[i], dp[Math.max(0, i - tickets[j])] + costs[j]);
             }
         }
 
-        return l;
+        return dp[max - 1];
     }
 }
