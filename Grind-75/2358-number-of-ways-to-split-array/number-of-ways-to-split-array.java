67c2447efcb38ca26863af29a98ece2fcc05038a
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-01-02 23:31:46 -0600

    Time: 2 ms (100.00%) | Memory: 57 MB (97.08%) - LeetSync

--- Grind-75/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
+++ Grind-75/2358-number-of-ways-to-split-array/number-of-ways-to-split-array.java
@@ -2,15 +2,16 @@ class Solution {
     public int waysToSplitArray(int[] nums) {
         int n = nums.length;
 
-        long[] prefix = new long[n];
-        prefix[0] = nums[0];
-        for (int i = 1; i < n; ++i) {
-            prefix[i] = prefix[i - 1] + nums[i];
+        long sum = 0;
+        for (int k : nums) {
+            sum += k;
         }
 
         int count = 0;
+        long cur = 0;
         for (int i = 0; i < n - 1; ++i) {
-            if (prefix[i] >= prefix[n - 1] - prefix[i]) {
+            cur += nums[i];
+            if (cur >= sum - cur) {
                 ++count;
             }
         }
