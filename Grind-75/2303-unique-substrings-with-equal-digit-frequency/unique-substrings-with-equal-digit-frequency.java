8b42141e875a81c96061e695624ccf4b4a16a1d1
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-01-08 16:57:28 -0600

    Time: 47 ms (94.92%) | Memory: 45.6 MB (74.58%) - LeetSync

--- Grind-75/2303-unique-substrings-with-equal-digit-frequency/unique-substrings-with-equal-digit-frequency.java
+++ Grind-75/2303-unique-substrings-with-equal-digit-frequency/unique-substrings-with-equal-digit-frequency.java
@@ -1,25 +1,26 @@
 class Solution {
     public int equalDigitFrequency(String s) {
         Set<Integer> vis = new HashSet<>();
-        int mod = (int) 10e7;
-        int prime = 31;
+
         for (int start = 0; start < s.length(); ++start) {
-            int[] freq = new int[10];
             int hash = 0;
-            int max = 0;
             int unique = 0;
+            int max = 0;
+            int[] freq = new int[10];
+
             for (int end = start; end < s.length(); ++end) {
                 int digit = s.charAt(end) - '0';
                 max = Math.max(max, ++freq[digit]);
-                if (freq[digit] == 1) {
-                    ++unique;
-                }
-                hash = (prime * hash + (digit + 1)) % mod;
+                if (freq[digit] == 1) ++unique;
+
+                hash = (31 * hash + digit + 1) % ((int) 10e7);
+
                 if (max * unique == end - start + 1) {
                     vis.add(hash);
                 }
             }
         }
+
         return vis.size();
     }
 }
