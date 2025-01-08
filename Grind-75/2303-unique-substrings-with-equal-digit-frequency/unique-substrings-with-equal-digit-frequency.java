31bfe3aa882dfb733fb95e94023b74700630da66
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-01-08 12:17:06 -0600

    Time: 619 ms (10.17%) | Memory: 45.1 MB (96.61%) - LeetSync

---
+++ Grind-75/2303-unique-substrings-with-equal-digit-frequency/unique-substrings-with-equal-digit-frequency.java
@@ -0,0 +1,75 @@
+class Solution {
+    public int equalDigitFrequency(String s) {
+        // return helper(s, 0, 0, new StringBuilder(), new HashSet<>(), new int[10]);
+        int count = 0;
+        for (int n = 1; n <= s.length(); ++n) {
+            count += helper(s, n);
+        }
+
+        return count;
+    }
+
+    private int helper(String s, int n) {
+        int count = 0;
+        int[] freq = new int[10];
+        Set<String> seen = new HashSet<>();
+
+        for (int i = 0; i < n - 1; ++i) {
+            ++freq[s.charAt(i) - '0'];
+        }
+
+        // if (valid(freq)) {
+        //     seen.add(s.substring(0, n));
+        //     ++count;
+        // }
+        for (int i = 0; i <= s.length() - n; ++i) {
+            ++freq[s.charAt(i + n - 1) - '0'];
+            if (!seen.contains(s.substring(i, i + n)) && valid(freq)) {
+                seen.add(s.substring(i, i + n));
+                ++count;
+            }
+            --freq[s.charAt(i) - '0'];
+        }
+
+        return count;
+    }
+
+    // private int helper(String s, int l, int r, StringBuilder sb, Set<String> seen, int[] freq) {
+    //     int n = 0;
+    //     if (r < s.length() - 1) {
+    //         ++freq[s.charAt(r) - '0'];
+    //         sb.append(s.charAt(r));
+    //         if (!seen.contains(sb.toString())) {
+    //             n += valid(freq) ? 1 : 0;
+    //             seen.add(sb.toString());
+    //         }
+    //         n += helper(s, l, r + 1, sb, seen, freq);
+    //         sb.deleteCharAt(sb.length() - 1);
+    //         --freq[s.charAt(r) - '0'];
+    //     }
+    //     if (l < r - 1) {
+    //         ++freq[s.charAt(l) - '0'];
+    //         sb.append(s.charAt(l));
+    //         if (!seen.contains(sb.toString())) {
+    //             n += valid(freq) ? 1 : 0;
+    //             seen.add(sb.toString());
+    //         }
+    //         n += helper(s, l + 1, r, sb, seen, freq);
+    //         sb.deleteCharAt(sb.length() - 1);
+    //         --freq[s.charAt(l) - '0'];
+    //     }
+    //     System.out.println(n);
+    //     return n;
+    // }
+
+    private boolean valid(int[] freq) {
+        int n = 0;
+        for (int i : freq) {
+            if (i == 0 || i == n) continue;
+            if (n == 0) n = i;
+            else return false;
+        }
+        System.out.println(Arrays.toString(freq));
+        return true;
+    }
+}
