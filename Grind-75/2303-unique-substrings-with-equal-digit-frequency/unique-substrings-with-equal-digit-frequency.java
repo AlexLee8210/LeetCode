0300c2a940d554257c0e3bb1c35fe1a312acaaa6
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-01-08 16:52:23 -0600

    Time: 43 ms (99.15%) | Memory: 45.2 MB (95.76%) - LeetSync

--- Grind-75/2303-unique-substrings-with-equal-digit-frequency/unique-substrings-with-equal-digit-frequency.java
+++ Grind-75/2303-unique-substrings-with-equal-digit-frequency/unique-substrings-with-equal-digit-frequency.java
@@ -1,75 +1,25 @@
 class Solution {
     public int equalDigitFrequency(String s) {
-        // return helper(s, 0, 0, new StringBuilder(), new HashSet<>(), new int[10]);
-        int count = 0;
-        for (int n = 1; n <= s.length(); ++n) {
-            count += helper(s, n);
-        }
-
-        return count;
-    }
-
-    private int helper(String s, int n) {
-        int count = 0;
-        int[] freq = new int[10];
-        Set<String> seen = new HashSet<>();
-        
-        StringBuilder sb = new StringBuilder();
-
-        for (int i = 0; i < n - 1; ++i) {
-            ++freq[s.charAt(i) - '0'];
-            sb.append(s.charAt(i));
-        }
-
-        for (int i = n - 1; i < s.length(); ++i) {
-            ++freq[s.charAt(i) - '0'];
-            sb.append(s.charAt(i));
-            if (!seen.contains(sb.toString()) && valid(freq)) {
-                seen.add(sb.toString());
-                ++count;
+        Set<Integer> vis = new HashSet<>();
+        int mod = (int) 10e7;
+        int prime = 31;
+        for (int start = 0; start < s.length(); ++start) {
+            int[] freq = new int[10];
+            int hash = 0;
+            int max = 0;
+            int unique = 0;
+            for (int end = start; end < s.length(); ++end) {
+                int digit = s.charAt(end) - '0';
+                max = Math.max(max, ++freq[digit]);
+                if (freq[digit] == 1) {
+                    ++unique;
+                }
+                hash = (prime * hash + (digit + 1)) % mod;
+                if (max * unique == end - start + 1) {
+                    vis.add(hash);
+                }
             }
-            --freq[s.charAt(i - n + 1) - '0'];
-            sb.deleteCharAt(0);
         }
-        return count;
-    }
-
-    // private int helper(String s, int l, int r, StringBuilder sb, Set<String> seen, int[] freq) {
-    //     int n = 0;
-    //     if (r < s.length() - 1) {
-    //         ++freq[s.charAt(r) - '0'];
-    //         sb.append(s.charAt(r));
-    //         if (!seen.contains(sb.toString())) {
-    //             n += valid(freq) ? 1 : 0;
-    //             seen.add(sb.toString());
-    //         }
-    //         n += helper(s, l, r + 1, sb, seen, freq);
-    //         sb.deleteCharAt(sb.length() - 1);
-    //         --freq[s.charAt(r) - '0'];
-    //     }
-    //     if (l < r - 1) {
-    //         ++freq[s.charAt(l) - '0'];
-    //         sb.append(s.charAt(l));
-    //         if (!seen.contains(sb.toString())) {
-    //             n += valid(freq) ? 1 : 0;
-    //             seen.add(sb.toString());
-    //         }
-    //         n += helper(s, l + 1, r, sb, seen, freq);
-    //         sb.deleteCharAt(sb.length() - 1);
-    //         --freq[s.charAt(l) - '0'];
-    //     }
-    //     System.out.println(n);
-    //     return n;
-    // }
-
-    private boolean valid(int[] freq) {
-        int n = 0;
-        for (int i : freq) {
-            if (i == 0 || i == n) continue;
-            if (n == 0) n = i;
-            else return false;
-        }
-        // System.out.println(Arrays.toString(freq));
-        return true;
+        return vis.size();
     }
 }
