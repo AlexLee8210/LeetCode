f20cf4f51e96245d936e8a274d00993d78cdb650
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-01-08 23:57:18 -0600

    Time: 66 ms (87.82%) | Memory: 45.6 MB (41.12%) - LeetSync

--- Grind-75/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
+++ Grind-75/2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -1,8 +1,6 @@
 class Solution {
     public int countPalindromicSubsequence(String s) {
         int[][] pos = new int[26][];
-        // Set<Integer> vis = new HashSet<>();
-        // int[] prefix = new int[s.length() + 1];
         int count = 0;
 
         for (int i = 0; i < s.length(); ++i) {
@@ -12,13 +10,10 @@ class Solution {
             } else {
                 pos[c][1] = i;
             }
-            // prefix[i + 1] = prefix[i] + (vis.contains(c) ? 0 : 1);
-            // vis.add(c);
         }
 
         for (int[] arr : pos) {
             if (arr == null || arr[1] <= arr[0] + 1) continue;
-            // Set<Character> vis = new HashSet<>();
             boolean[] vis = new boolean[26];
             int start = arr[0];
             int end = arr[1];
@@ -27,7 +22,6 @@ class Solution {
                 if (!vis[s.charAt(i) - 'a']) ++count;
                 vis[s.charAt(i) - 'a'] = true;
             }
-            // count += prefix[end] - prefix[start + 1] + 1;
         }
 
         return count;
