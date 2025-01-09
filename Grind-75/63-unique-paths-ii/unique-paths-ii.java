9b80ff96787b8b48c49651122a8067346c8deda1
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-01-09 11:49:21 -0600

    Time: 6 ms (4.85%) | Memory: 42.1 MB (16.25%) - LeetSync

---
+++ Grind-75/63-unique-paths-ii/unique-paths-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int uniquePathsWithObstacles(int[][] obstacleGrid) {
+        if (obstacleGrid[0][0] == 1) return 0;
+        int m = obstacleGrid.length;
+        int n = obstacleGrid[0].length;
+        int[][] dp = new int[m][n];
+        dp[0][0] = 1;
+
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (obstacleGrid[r][c] == 1) continue;
+                if (r == 0 && c == 0) continue;
+                if (r == 0) dp[r][c] = dp[r][c - 1];
+                else if (c == 0) dp[r][c] = dp[r - 1][c];
+                else {
+                    dp[r][c] = dp[r - 1][c] + dp[r][c - 1];
+                }
+            }
+        }
+        for (int[] row : dp) {
+            System.out.println(Arrays.toString(row));
+        }
+        return dp[m - 1][n - 1];
+    }
+}
