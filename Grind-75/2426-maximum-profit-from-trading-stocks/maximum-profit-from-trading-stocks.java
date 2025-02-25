898ad4f640c6903a0e6d208356b1772f952064e8
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-02-25 01:20:34 -0600

    Time: 13 ms (85.15%) | Memory: 43.3 MB (78.17%) - LeetSync

--- Grind-75/2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.java
+++ Grind-75/2426-maximum-profit-from-trading-stocks/maximum-profit-from-trading-stocks.java
@@ -1,20 +1,16 @@
 class Solution {
     public int maximumProfit(int[] present, int[] future, int budget) {
         int n = present.length;
-        int[][] dp = new int[n + 1][budget + 1];
+        int[] dp = new int[budget + 1];
 
         for (int i = 0; i < n; ++i) {
             int revenue = future[i] - present[i];
-            for (int b = 0; b <= budget; ++b) {
-                if (b - present[i] < 0 || revenue <= 0) {
-                    dp[i + 1][b] = dp[i][b];
-                } else {
-                    dp[i + 1][b] = revenue + dp[i][b - present[i]];
-                    dp[i + 1][b] = Math.max(dp[i + 1][b], dp[i][b]);
-                }
+            for (int b = budget; b >= 0; --b) {
+                if (b - present[i] < 0 || revenue <= 0) continue;
+                dp[b] = Math.max(revenue + dp[b - present[i]], dp[b]);
             }
         }
 
-        return dp[n][budget];
+        return dp[budget];
     }
 }
