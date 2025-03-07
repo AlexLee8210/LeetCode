eb46e937c7c1f70d81aca7fc21d7b16d6b8b273d
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-03-07 10:57:59 -0600

    Time: 11 ms (14.13%) | Memory: 72.8 MB (5.35%) - LeetSync

--- Grind-75/207-course-schedule/course-schedule.java
+++ Grind-75/207-course-schedule/course-schedule.java
@@ -1,35 +1,33 @@
 class Solution {
     public boolean canFinish(int numCourses, int[][] prerequisites) {
-        int[] indegree = new int[numCourses]; // number of prereqs for course i
-        ArrayList<Integer>[] adj = new ArrayList[numCourses];
-        int total = 0;
-        for (int i = 0; i < numCourses; i++) {
-            adj[i] = new ArrayList<>();
-        }
+        int[] indegree = new int[numCourses];
+        ArrayList<Integer>[] requisites = new ArrayList[numCourses];
+
         for (int[] prereq : prerequisites) {
-            adj[prereq[1]].add(prereq[0]);
-            indegree[prereq[0]]++;
-            total++;
+            ++indegree[prereq[0]];
+            if (requisites[prereq[1]] == null) {
+                requisites[prereq[1]] = new ArrayList<>(numCourses);
+            }
+            requisites[prereq[1]].add(prereq[0]);
         }
 
-        Queue<Integer> q = new LinkedList<>();
-        for (int i = 0; i < numCourses; i++) {
-            if (indegree[i] == 0) {
-                q.offer(i);
+        Deque<Integer> dq = new LinkedList<>();
+
+        for (int i = 0; i < numCourses; ++i) {
+            if (indegree[i] == 0) dq.offer(i);
+        }
+
+        int courses = 0;
+
+        while (!dq.isEmpty()) {
+            int course = dq.poll();
+            ++courses;
+            if (requisites[course] == null) continue;
+            for (int req : requisites[course]) {
+                if (--indegree[req] == 0) dq.offer(req);
             }
         }
 
-        while(!q.isEmpty()) {
-            int course = q.poll();
-            for (int i : adj[course]) {
-                indegree[i]--;
-                total--;
-                if (indegree[i] == 0) {
-                    q.offer(i);
-                }
-            }
-        }
-
-        return total == 0;
+        return courses == numCourses;
     }
 }
