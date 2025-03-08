ba9577192a1f4cca1ff1ca5b7e398f4139a7f9d3
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-03-08 13:29:22 -0600

    Time: 0 ms (100.00%) | Memory: 44.4 MB (82.82%) - LeetSync

--- Grind-75/142-linked-list-cycle-ii/linked-list-cycle-ii.java
+++ Grind-75/142-linked-list-cycle-ii/linked-list-cycle-ii.java
@@ -11,20 +11,19 @@
  */
 public class Solution {
     public ListNode detectCycle(ListNode head) {
-        if (head == null) return null;
-        if (head.next == null) return head.equals(head.next) ? head : null;
-
-        ListNode slow = head.next;
-        ListNode fast = head.next.next;
-        while (fast != null && fast.next != null && !slow.equals(fast)) {
+        ListNode slow = head, fast = head;
+        
+        while (fast != null && fast.next != null) {
             slow = slow.next;
             fast = fast.next.next;
+            
+            if (slow == fast) break;
         }
 
         if (fast == null || fast.next == null) return null;
 
         slow = head;
-        while (!slow.equals(fast)) {
+        while (slow != fast) {
             slow = slow.next;
             fast = fast.next;
         }
