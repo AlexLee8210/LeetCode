205c2dc3a1a110b32e025f9cebd9395d4550d45e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-03-07 18:32:08 -0600

    Time: 2 ms (74.67%) | Memory: 61.2 MB (91.60%) - LeetSync

--- Grind-75/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
+++ Grind-75/121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.java
@@ -1,14 +1,12 @@
 class Solution {
     public int maxProfit(int[] prices) {
+        int min = prices[0];
         int profit = 0;
-        int buy = prices[0];
-        for (int i = 1; i < prices.length; i++) {
-            if (prices[i] < buy) {
-                buy = prices[i];
-            } else if (prices[i] - buy > profit) {
-                profit = prices[i] - buy;
-            }
+        for (int i = 1; i < prices.length; ++i) {
+            profit = Math.max(prices[i] - min, profit);
+            min = Math.min(prices[i], min);
         }
+
         return profit;
     }
 }
