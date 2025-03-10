1b49bd79047174a757208b4150c9fcd8903183b6
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-03-09 21:40:56 -0500

    Time: 4 ms (16.79%) | Memory: 45.6 MB (69.76%) - LeetSync

--- Grind-75/55-jump-game/jump-game.java
+++ Grind-75/55-jump-game/jump-game.java
@@ -2,8 +2,7 @@ class Solution {
     public boolean canJump(int[] nums) {
         int max = 0;
 
-        for (int i = 0; i < nums.length; ++i) {
-            if (max < i) return false; 
+        for (int i = 0; i <= Math.min(max, nums.length - 1); ++i) {
             max = Math.max(max, nums[i] + i);
         }
 
