8cf049e977e93784d35c3d3afd8c0849825d711a
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-03-09 21:29:32 -0500

    Time: 11 ms (91.64%) | Memory: 48.9 MB (43.13%) - LeetSync

--- Grind-75/347-top-k-frequent-elements/top-k-frequent-elements.java
+++ Grind-75/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,21 +1,27 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         HashMap<Integer, Integer> freq = new HashMap<>();
-        PriorityQueue<Integer> pq = new PriorityQueue<>(k + 1, (Integer a, Integer b) -> freq.get(a) - freq.get(b));
+        List<Integer>[] buckets = new List[nums.length + 1];
 
         for (int n : nums) {
             freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
 
         for (int n : freq.keySet()) {
-            pq.offer(n);
-            if (pq.size() == k + 1) pq.poll();
+            int count = freq.get(n);
+            if (buckets[count] == null) buckets[count] = new ArrayList<>();
+            buckets[count].add(n);
         }
 
         int[] res = new int[k];
-
-        for (int i = 0; i < k; ++i) {
-            res[i] = pq.poll();
+        
+        int idx = 0;
+        for (int i = buckets.length - 1; i >= 0; --i) {
+            if (buckets[i] == null) continue;
+            for (int j = 0; j < buckets[i].size() && idx < k; ++j) {
+                res[idx] = buckets[i].get(j);
+                ++idx;
+            }
         }
 
         return res;
