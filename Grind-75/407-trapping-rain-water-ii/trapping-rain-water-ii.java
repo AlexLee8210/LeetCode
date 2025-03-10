ae73aa7f2e2d2ffd5959e2cf63c95cce713eb038
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-03-09 23:47:05 -0500

    Time: 75 ms (5.39%) | Memory: 50 MB (5.14%) - LeetSync

--- Grind-75/407-trapping-rain-water-ii/trapping-rain-water-ii.java
+++ Grind-75/407-trapping-rain-water-ii/trapping-rain-water-ii.java
@@ -1,62 +1,52 @@
 class Solution {
-    private class Node {
-        int row, col, height;
-
-        public Node(int row, int col, int height) {
-            this.row = row;
-            this.col = col;
-            this.height = height;
-        }
-    }
-
-    private boolean inBounds(int r, int c, int m, int n) {
-        return 0 <= r && r < m && 0 <= c && c < n;
-    }
-
     public int trapRainWater(int[][] heightMap) {
-        if (heightMap == null || heightMap.length < 3 || heightMap[0].length < 3) {
-            return 0;
-        }
-        int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
         int m = heightMap.length;
         int n = heightMap[0].length;
 
+        int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
+
+        PriorityQueue<int[]> pq = new PriorityQueue<>((int[] a, int[] b) -> heightMap[a[0]][a[1]] - heightMap[b[0]][b[1]]);
+
         int total = 0;
 
-        boolean[][] vis = new boolean[m][n];
-        PriorityQueue<Node> pq = new PriorityQueue<>((Node a, Node b) -> {
-            return a.height - b.height;
-        });
-
-        for (int c = 0; c < n; ++c) {
-            vis[0][c] = true;
-            vis[m - 1][c] = true;
-            pq.offer(new Node(0, c, heightMap[0][c]));
-            pq.offer(new Node(m - 1, c, heightMap[m - 1][c]));
-        }
-
         for (int r = 0; r < m; ++r) {
-            vis[r][0] = true;
-            vis[r][n - 1] = true;
-            pq.offer(new Node(r, 0, heightMap[r][0]));
-            pq.offer(new Node(r, n - 1, heightMap[r][n - 1]));
+            pq.offer(new int[]{r, 0});
+            pq.offer(new int[]{r, n - 1});
+        }
+        for (int c = 1; c < n - 1; ++c) {
+            pq.offer(new int[]{0, c});
+            pq.offer(new int[]{m - 1, c});
         }
 
-        while (!pq.isEmpty()) {
-            Node cur = pq.poll();
+        boolean[][] vis = new boolean[m][n];
+        int height = 0;
+
+        while(!pq.isEmpty()) {
+            int[] cell = pq.poll();
+            if (vis[cell[0]][cell[1]]) continue;
+            vis[cell[0]][cell[1]] = true;
+            
+            if (height < heightMap[cell[0]][cell[1]]) {
+                height = heightMap[cell[0]][cell[1]];
+            } else {
+                total += height - heightMap[cell[0]][cell[1]];
+                heightMap[cell[0]][cell[1]] = height;
+            }
+
             for (int[] dir : dirs) {
-                int newRow = cur.row + dir[0];
-                int newCol = cur.col + dir[1];
+                int nextRow = cell[0] + dir[0];
+                int nextCol = cell[1] + dir[1];
 
-                if (!inBounds(newRow, newCol, m, n) || vis[newRow][newCol]) continue;
+                if (!inBounds(nextRow, nextCol, m, n)) continue;
 
-                vis[newRow][newCol] = true;
-                pq.offer(new Node(newRow, newCol, Math.max(cur.height, heightMap[newRow][newCol])));
-
-                total += Math.max(0, cur.height - heightMap[newRow][newCol]);
+                pq.offer(new int[]{nextRow, nextCol});
             }
         }
 
         return total;
+    }
+
+    private boolean inBounds(int r, int c, int m, int n) {
+        return 0 <= r && r < m && 0 <= c && c < n;
     }
 }
