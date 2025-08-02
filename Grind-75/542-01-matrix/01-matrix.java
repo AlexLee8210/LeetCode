7d3ac0a1a921cbd4416cbb69e99589c214351576
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-08-02 16:22:32 -0500

    Time: 7 ms (98.53%) | Memory: 48.7 MB (5.56%) - LeetSync

--- Grind-75/542-01-matrix/01-matrix.java
+++ Grind-75/542-01-matrix/01-matrix.java
@@ -1,25 +1,30 @@
 class Solution {
     public int[][] updateMatrix(int[][] mat) {
-        int m = mat.length, n = mat[0].length;
-        int INF = m + n;
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
+        int m = mat.length;
+        int n = mat[0].length;
+        int[][] dist = new int[m][n];
+        int MAX = m + n;
+
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
                 if (mat[r][c] == 0) continue;
-                int top = INF, left = INF;
-                if (r > 0) top = mat[r - 1][c];
-                if (c > 0) left = mat[r][c - 1];
-                mat[r][c] = Math.min(top, left) + 1;
+                if (r == 0 && c == 0) dist[r][c] = MAX;
+                else if (r == 0) dist[r][c] = (dist[r][c - 1] == MAX) ? MAX : dist[r][c - 1] + 1;
+                else if (c == 0) dist[r][c] = (dist[r - 1][c] == MAX) ? MAX : dist[r - 1][c] + 1;
+                else dist[r][c] = Math.min(dist[r - 1][c], dist[r][c - 1]) + 1;
             }
         }
-        for (int r = m - 1; r >= 0; r--) {
-            for (int c = n - 1; c >= 0; c--) {
+
+        for (int r = m - 1; r >= 0; --r) {
+            for (int c = n - 1; c >= 0; --c) {
                 if (mat[r][c] == 0) continue;
-                int bottom = INF, right = INF;
-                if (r < m - 1) bottom = mat[r + 1][c];
-                if (c < n - 1) right = mat[r][c + 1];
-                mat[r][c] = Math.min(mat[r][c], Math.min(bottom, right) + 1);
+                if (r == m - 1 && c == n - 1) continue;
+                if (r == m - 1) dist[r][c] = Math.min(dist[r][c], dist[r][c + 1] + 1);
+                else if (c == n - 1) dist[r][c] = Math.min(dist[r][c], dist[r + 1][c] + 1);
+                else dist[r][c] = Math.min(dist[r][c], Math.min(dist[r + 1][c], dist[r][c + 1]) + 1);
             }
         }
-        return mat;
+
+        return dist;
     }
 }
