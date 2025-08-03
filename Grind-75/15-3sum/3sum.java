3145678de446b89e507bbdfe52798aeb6427df4b
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-08-03 00:20:38 -0500

    Time: 1552 ms (5.45%) | Memory: 54.5 MB (5.03%) - LeetSync

--- Grind-75/15-3sum/3sum.java
+++ Grind-75/15-3sum/3sum.java
@@ -1,29 +1,22 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
-        List<List<Integer>> res = new ArrayList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (i != 0 && nums[i] == nums[i - 1]) continue;
-            int l = i + 1, r = nums.length - 1;
-            while (l < r) {
-                int sum = nums[l] + nums[r];
-                if (sum < -nums[i]) {
-                    l++;
-                } else if (sum > -nums[i]) {
-                    r--;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
-                    l++;
-                    r--;
-                    while (l < r && nums[l] == nums[l - 1]) {
-                        l++;
-                    }
-                    while (l < r && nums[r] == nums[r + 1]) {
-                        r--;
-                    }
+        Set<List<Integer>> trips = new HashSet<>();
+        for (int i = 0; i < nums.length - 2; ++i) {
+            HashMap<Integer, Integer> map = new HashMap<>();
+            for (int j = i + 1; j < nums.length; ++j) {
+                int numK = - (nums[i] + nums[j]);
+                if (map.containsKey(numK)) {
+                    ArrayList<Integer> list = new ArrayList<>();
+                    list.add(nums[i]);
+                    list.add(nums[j]);
+                    list.add(numK);
+                    Collections.sort(list);
+                    trips.add(list);
                 }
+                map.put(nums[j], j);
             }
         }
-        return res;
+
+        return new ArrayList<>(trips);
     }
 }
