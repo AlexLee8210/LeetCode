4df8169e3e746f9b7d5110078e849d809f9389f3
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-08-25 22:21:23 -0500

    Time: 12 ms (92.68%) | Memory: 44 MB (98.30%) - LeetSync

--- Grind-75/322-coin-change/coin-change.java
+++ Grind-75/322-coin-change/coin-change.java
@@ -1,23 +1,18 @@
 class Solution {
     public int coinChange(int[] coins, int amount) {
-        if (amount == 0) return 0;
-        Arrays.sort(coins);
-        if (coins[0] > amount) return -1;
-        int[] amounts = new int[amount + 1];
-        for (int i = 1; i < coins[0]; i++) {
-            amounts[i] = -1;
-        }
-        for (int i = coins[0]; i < amounts.length; i++) {
+        int[] dp = new int[amount + 1];
+
+        for (int i = 1; i <= amount; ++i) {
             int min = Integer.MAX_VALUE;
-            for (int val : coins) {
-                int target = i - val;
-                if (target < 0 || amounts[target] == -1) {
-                    continue;
-                }
-                min = Math.min(amounts[target] + 1, min);
+            for (int coin : coins) {
+                if (i - coin < 0 || dp[i - coin] == -1) continue;
+
+                min = Math.min(min, dp[i - coin] + 1);
             }
-            amounts[i] = min == Integer.MAX_VALUE ? -1 : min;
+
+            dp[i] = min == Integer.MAX_VALUE ? -1 : min;
         }
-        return amounts[amount];
+
+        return dp[amount];
     }
 }
