5ef726dad3e4c8008ba506dd61f9803ce06e46b8
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-08-25 22:31:40 -0500

    Time: 2 ms (87.11%) | Memory: 55.5 MB (50.06%) - LeetSync

--- Grind-75/238-product-of-array-except-self/product-of-array-except-self.java
+++ Grind-75/238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,26 +1,17 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] prefix = new int[nums.length];
-        int[] postfix = new int[nums.length];
-        prefix[0] = 1;
-        for (int i = 1; i < prefix.length; i++) {
-            prefix[i] = prefix[i - 1] * nums[i - 1];
+        int[] answer = new int[nums.length];
+        answer[0] = 1;
+        for (int i = 0; i < nums.length - 1; ++i) {
+            answer[i + 1] = answer[i] * nums[i];
         }
-        postfix[postfix.length - 1] = 1;
-        for (int i = postfix.length - 2; i >= 0; i--) {
-            postfix[i] = postfix[i + 1] * nums[i + 1];
+        
+        int prod = 1;
+        for (int i = nums.length - 1; i > 0; --i) {
+            prod *= nums[i];
+            answer[i - 1] *= prod;
         }
-        int[] res = new int[nums.length];
-        for (int i = 0; i < res.length; i++) {
-            res[i] = prefix[i] * postfix[i];
-        }
-        // System.out.println(Arrays.toString(prefix));
-        // System.out.println(Arrays.toString(postfix));
-        /*
-        1   2   3   4
-        1   1   2   6
-        24  12  4   1
-        */
-        return res;
+
+        return answer;
     }
 }
