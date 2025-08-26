ae55cb7247ff7d4aaa7a2b2bb1e5310b22dc406e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-08-26 15:13:17 -0500

    Time: 1 ms (99.52%) | Memory: 57.2 MB (42.64%) - LeetSync

--- Grind-75/53-maximum-subarray/maximum-subarray.java
+++ Grind-75/53-maximum-subarray/maximum-subarray.java
@@ -1,15 +1,14 @@
 class Solution {
     public int maxSubArray(int[] nums) {
-        int max = nums[0];
-        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        int cur = 0;
         for (int i = 0; i < nums.length; ++i) {
-            sum += nums[i];
-            max = Math.max(max, sum);
-            if (sum < 0) {
-                sum = 0;
+            cur += nums[i];
+            max = Math.max(max, cur);
+            if (cur < 0) {
+                cur = 0;
             }
         }
-
         return max;
     }
 }
