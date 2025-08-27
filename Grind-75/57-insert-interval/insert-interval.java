258f44af1f52b41adc7a9aeead3ad4cd200defbf
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-08-27 10:39:47 -0500

    Time: 1 ms (98.85%) | Memory: 45.3 MB (14.67%) - LeetSync

--- Grind-75/57-insert-interval/insert-interval.java
+++ Grind-75/57-insert-interval/insert-interval.java
@@ -1,24 +1,29 @@
 class Solution {
     public int[][] insert(int[][] intervals, int[] newInterval) {
-        ArrayList<int[]> res = new ArrayList<>();
+        List<int[]> res = new ArrayList<>();
         int idx = 0;
-         
         while (idx < intervals.length && intervals[idx][1] < newInterval[0]) {
-            res.add(intervals[idx++]);
+            res.add(intervals[idx]);
+            ++idx;
         }
 
-        // newInterval[0] = Math.min(intervals[idx][0], newInterval[0]);
-        while (idx < intervals.length && intervals[idx][1] >= newInterval[0] && newInterval[1] >= intervals[idx][0]) {
-            newInterval[0] = Math.min(intervals[idx][0], newInterval[0]);
-            newInterval[1] = Math.max(intervals[idx++][1], newInterval[1]);
+        if (idx == intervals.length) {
+            res.add(newInterval);
+            return res.toArray(new int[res.size()][]);
         }
 
+        newInterval[0] = Math.min(intervals[idx][0], newInterval[0]);
+        while (idx < intervals.length && newInterval[1] >= intervals[idx][0]) {
+            newInterval[1] = Math.max(newInterval[1], intervals[idx][1]);
+            ++idx;
+        }
         res.add(newInterval);
-         
+
         while (idx < intervals.length) {
-            res.add(intervals[idx++]);
+            res.add(intervals[idx]);
+            ++idx;
         }
 
-        return res.toArray(new int[res.size()][2]);
+        return res.toArray(new int[res.size()][]);
     }
 }
