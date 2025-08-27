96ec1726e085b58859c7e26f5c478e3ac2464307
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-08-27 12:30:29 -0500

    Time: 5 ms (31.92%) | Memory: 52.4 MB (14.08%) - LeetSync

--- Grind-75/200-number-of-islands/number-of-islands.java
+++ Grind-75/200-number-of-islands/number-of-islands.java
@@ -1,47 +1,39 @@
 class Solution {
-    private int[][] dirs = new int[][]{{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-
+    private int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     public int numIslands(char[][] grid) {
-        int m = grid.length;
-        int n = grid[0].length;
-        int count = 0;
-
-        for (int r = 0; r < m; ++r) {
-            for (int c = 0; c < n; ++c) {
+        int num = 0;
+        for (int r = 0; r < grid.length; ++r) {
+            for (int c = 0; c < grid[0].length; ++c) {
                 if (grid[r][c] == '1') {
-                    ++count;
                     bfs(grid, r, c);
+                    ++num;
                 }
             }
         }
 
-        return count;
+        return num;
     }
 
     private void bfs(char[][] grid, int r, int c) {
-        int m = grid.length;
-        int n = grid[0].length;
-        grid[r][c] = '0';
+        Queue<int[]> q = new LinkedList<>();
+        q.offer(new int[]{r, c});
 
-        Queue<Pair<Integer, Integer>> q = new LinkedList<>();
-        q.add(new Pair<>(r, c));
-        
         while (!q.isEmpty()) {
-            Pair<Integer, Integer> pos = q.poll();
-
+            int[] point = q.poll();
+            if (grid[point[0]][point[1]] == '0') continue;
+            grid[point[0]][point[1]] = '0';
             for (int[] dir : dirs) {
-                int newR = pos.getKey() + dir[0];
-                int newC = pos.getValue() + dir[1];
-                
-                if (!inBounds(newR, newC, m, n) || grid[newR][newC] == '0') continue;
-                grid[newR][newC] = '0';
+                int[] newPoint = new int[]{point[0] + dir[0], point[1] + dir[1]};
+                if (!isValidPoint(newPoint, grid.length, grid[0].length)) continue;
+                if (grid[newPoint[0]][newPoint[1]] != '1') continue;
 
-                q.offer(new Pair<>(newR, newC));
+                q.offer(newPoint);
             }
         }
     }
 
-    private boolean inBounds(int r, int c, int m, int n) {
-        return 0 <= r && r < m && 0 <= c && c < n;
+    private boolean isValidPoint(int[] point, int m, int n) {
+        return point[0] >= 0 && point[0] < m
+            && point[1] >= 0 && point[1] < n;
     }
 }
