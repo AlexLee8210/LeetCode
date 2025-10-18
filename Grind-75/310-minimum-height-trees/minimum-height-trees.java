5ee5778ca80ca2e1d63e3cfbf5de44fec0a859af
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-10-18 13:31:48 -0500

    Time: 16 ms (77.76%) | Memory: 56.9 MB (41.04%) - LeetSync

--- Grind-75/310-minimum-height-trees/minimum-height-trees.java
+++ Grind-75/310-minimum-height-trees/minimum-height-trees.java
@@ -1,103 +1,49 @@
 class Solution {
     public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-        if (n == 1) {
-            ArrayList<Integer> mht = new ArrayList<>();
-            mht.add(0);
-            return mht;
-        }
-
-        HashSet<Integer>[] adj = new HashSet[n];
+        List<Integer> res = new ArrayList<>();
         int[] deg = new int[n];
-        for (int i = 0; i < n; i++) {
-            adj[i] = new HashSet<>();
-        }
-        for (int[] edge : edges) {
-            adj[edge[0]].add(edge[1]);
-            adj[edge[1]].add(edge[0]);
-            deg[edge[0]]++;
-            deg[edge[1]]++;
-        }
+        List<Integer>[] adj = new List[n];
 
-        ArrayList<Integer> leaves = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            if (deg[i] == 1) leaves.add(i);
-        }
-        while (n > 2) {
-            n -= leaves.size();
-            ArrayList<Integer> newLeaves = new ArrayList<>();
-            for (int i = leaves.size() - 1; i >= 0; i--) {
-                int leaf = leaves.get(i);
-                int neighbor = adj[leaf].iterator().next();
-                adj[neighbor].remove(leaf);
-                deg[neighbor]--;
-                if (deg[neighbor] == 1) newLeaves.add(neighbor);
+        for (int i = 0; i < edges.length; ++i) {
+            ++deg[edges[i][0]];
+            ++deg[edges[i][1]];
+            if (adj[edges[i][0]] == null) {
+                adj[edges[i][0]] = new ArrayList<>();
             }
-            leaves = newLeaves;
+            if (adj[edges[i][1]] == null) {
+                adj[edges[i][1]] = new ArrayList<>();
+            }
+            adj[edges[i][0]].add(edges[i][1]);
+            adj[edges[i][1]].add(edges[i][0]);
         }
-        return leaves;
+
+        Queue<Integer> q = new LinkedList<>();
+        for (int i = 0; i < n; ++i) {
+            if (deg[i] <= 1) {
+                q.offer(i);
+            }
+        }
+
+        while(n > 2) {
+            int size = q.size();
+            n -= size;
+
+            for (int i = 0; i < size; ++i) {
+                int leaf = q.poll();
+                if (adj[leaf] == null) continue;
+                for (int dst : adj[leaf]) {
+                    --deg[dst];
+                    if (deg[dst] == 1) {
+                        q.offer(dst);
+                    }
+                }
+            }
+        }
+
+        while (!q.isEmpty()) {
+            res.add(q.poll());
+        }
+
+        return res;
     }
-    // public List<Integer> findMinHeightTrees(int n, int[][] edges) {
-    //     ArrayList<Integer>[] graph = new ArrayList[n];
-    //     int[][] dp = new int[n][n];
-
-    //     for (int[] edge : edges) {
-    //         if (graph[edge[0]] == null) {
-    //             graph[edge[0]] = new ArrayList<>();
-    //         }
-    //         graph[edge[0]].add(edge[1]);
-    //         dp[edge[0]][edge[1]] = 1;
-    //         if (graph[edge[1]] == null) {
-    //             graph[edge[1]] = new ArrayList<>();
-    //         }
-    //         graph[edge[1]].add(edge[0]);
-    //         dp[edge[1]][edge[0]] = 1;
-    //     }
-
-    //     ArrayList<Integer> res = new ArrayList<>();
-    //     int minHeight = Integer.MAX_VALUE;
-    //     for (int i = 0; i < n; i++) {
-    //         // find the max distance from i to any node
-    //         int height = Integer.MIN_VALUE;
-    //         for (int j = 0; j < n; j++) {
-    //             if (i == j) continue;
-    //             if (dp[i][j] == 0) {
-    //                 height = Math.max(height, bfs(graph, dp, i, j));
-    //             } else {
-    //                 height = Math.max(height, dp[i][j]);
-    //             }
-    //         }
-    //         if (height < minHeight) {
-    //             minHeight = height;
-    //             res = new ArrayList<>();
-    //             res.add(i);
-    //         } else if (height == minHeight) {
-    //             res.add(i);
-    //         }
-    //     }
-    //     return res;
-    // }
-
-    // private int bfs(ArrayList<Integer>[] graph, int[][] dp, int src, int dest) {
-    //     Queue<Integer> q = new LinkedList<>();
-    //     q.offer(src);
-    //     while (!q.isEmpty()) {
-    //         int node = q.poll();
-    //         if (dp[node][dest] != 0) {
-    //             dp[src][dest] = dp[src][node] + dp[node][dest];
-    //             return dp[src][dest];
-    //         }
-    //         for (int neighbor : graph[node]) {
-    //             if (dp[src][neighbor] == 0) {
-    //                 dp[src][neighbor] = dp[src][node] + 1;
-    //             } else {
-    //                 dp[src][neighbor] = Math.min(dp[src][node] + 1, dp[src][neighbor]);
-    //             }
-    //             if (neighbor == dest) {
-    //                 return dp[src][dest];
-    //             }
-    //             q.offer(neighbor);
-    //         }
-    //     }
-    //     return -1;
-    // }
 }
