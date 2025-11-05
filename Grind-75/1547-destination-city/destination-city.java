228c414d1fedd86c29b06d06ecc3de84bb4fd200
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-11-05 15:30:41 -0600

    Time: 2 ms (99.91%) | Memory: 45 MB (5.58%) - LeetSync

---
+++ Grind-75/1547-destination-city/destination-city.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String destCity(List<List<String>> paths) {
+        Set<String> srcCities = new HashSet<>();
+        for (List<String> path : paths) {
+            srcCities.add(path.get(0));
+        }
+        for (List<String> path : paths) {
+            if (!srcCities.contains(path.get(1))) return path.get(1);
+        }
+        return "invalid";
+    }
+}
