f30c72f0949e8c9e098e22ba63bcef69fbad2463
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-11-12 14:33:56 -0600

    Time: 3 ms (78.87%) | Memory: 44 MB (26.53%) - LeetSync

--- Grind-75/752-ip-to-cidr/ip-to-cidr.java
+++ Grind-75/752-ip-to-cidr/ip-to-cidr.java
@@ -1,54 +1,43 @@
 class Solution {
-
-    private static final int NUM_GROUPS = 4;
-    private static final int GROUP_SIZE = 8; // in bits
-
     public List<String> ipToCIDR(String ip, int n) {
-        long cur = ipToLong(ip);
-        List<String> res = new ArrayList<>();
-
+        List<String> blocks = new ArrayList<>();
+        long val = ipToLong(ip);
         while (n > 0) {
-            long zeros = cur & (-cur);
-            if (zeros == 0) zeros = (long) 1 << (NUM_GROUPS * GROUP_SIZE);
-            while (zeros > n) {
-                zeros >>= 1;
+            long sig = val & (-val);
+            if (sig == 0) sig = 1L << 32;
+            while (sig > n) {
+                sig >>= 1;
             }
-            res.add(ipToString(cur, (int) zeros));
-            cur += zeros;
-            n -= zeros;
+            blocks.add(longToCIDR(val, sig));
+            val += sig;
+            n -= sig;
+        }
+        return blocks;
+    }
+
+    private long ipToLong(String ip) {
+        String[] groups = ip.split("\\.");
+        long res = 0;
+        for (int i = 0; i < groups.length; ++i) {
+            res <<= 8;
+            res += Long.parseLong(groups[i]);
         }
         return res;
     }
 
-    private String ipToString(long ip, int zeros) {
-        int[] groups = new int[NUM_GROUPS];
-        for (int i = 0; i < NUM_GROUPS; ++i) {
-            groups[i] = (int) (ip & ((1 << GROUP_SIZE) - 1));
-            ip >>= GROUP_SIZE;
-        }
-
+    private String longToCIDR(long val, long sig) {
         StringBuilder sb = new StringBuilder();
-        for (int i = NUM_GROUPS - 1; i >= 0; --i) {
-            sb.append(groups[i]).append(".");
+        for (int i = 0; i < 4; ++i) {
+            sb.insert(0, ".").insert(0, val & 255);
+            val >>= 8;
         }
         sb.setCharAt(sb.length() - 1, '/');
-
-        int len = NUM_GROUPS * GROUP_SIZE + 1;
-        while (zeros > 0) {
-            zeros >>= 1;
-            --len;
+        int length = 33;
+        while (sig > 0) {
+            sig >>= 1;
+            --length;
         }
-        sb.append(len);
+        sb.append(length);
         return sb.toString();
-    }
-
-    private long ipToLong(String ip) {
-        long val = 0;
-        String[] groups = ip.split("\\.");
-        for (int i = 0; i < groups.length; ++i) {
-            val *= (1 << GROUP_SIZE);
-            val += Long.parseLong(groups[i]);
-        }
-        return val;
     }
 }
