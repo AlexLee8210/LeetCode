20919f10547922a5df2ee353a0536fe27e517b17
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-11-18 17:36:47 -0600

    Time: 0 ms (100.00%) | Memory: 42.9 MB (8.09%) - LeetSync

--- Grind-75/198-house-robber/house-robber.java
+++ Grind-75/198-house-robber/house-robber.java
@@ -1,28 +1,15 @@
 class Solution {
     public int rob(int[] nums) {
-        if (nums.length == 1) {
-            return nums[0];
-        }
-        int[] memo = new int[nums.length];
-        Arrays.fill(memo, -1);
-        int max = 0;
-        memo[0] = nums[0];
-        memo[1] = Math.max(nums[1], nums[0]);
-        for (int i = 2; i < nums.length; i++) {
-            memo[i] = Math.max(memo[i - 2] + nums[i], memo[i - 1]);
-        }
-        return memo[nums.length - 1];
-        // return helper(nums, 0, memo);
-    }
+        if (nums.length == 1) return nums[0];
 
-    // private int helper(int[] nums, int idx, int[] memo) {
-    //     if (idx >= nums.length) {
-    //         return 0;
-    //     }
-    //     if (memo[idx] > -1) {
-    //         return memo[idx];
-    //     }
-    //     memo[idx] = Math.max(helper(nums, idx + 1, memo), helper(nums, idx + 2, memo) + nums[idx]);
-    //     return memo[idx];
-    // }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+
+        for (int i = 2; i < nums.length; ++i) {
+            dp[i] = Math.max(dp[i - 2] + nums[i], dp[i - 1]);
+        }
+        
+        return dp[nums.length - 1];
+    }
 }
