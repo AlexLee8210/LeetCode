a13c9d7ea732c41ac2847e5fd91337cc19ff136a
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-11-18 22:49:47 -0600

    Time: 14 ms (44.17%) | Memory: 46.2 MB (32.73%) - LeetSync

--- Grind-75/438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
+++ Grind-75/438-find-all-anagrams-in-a-string/find-all-anagrams-in-a-string.java
@@ -1,26 +1,33 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        ArrayList<Integer> res = new ArrayList<>();
-        if (p.length() > s.length()) return res;
-
+        List<Integer> res = new ArrayList<>();
+        if (s.length() < p.length()) return res;
         int[] freq = new int[26];
-        for (int i = 0; i < p.length(); i++) {
-            freq[p.charAt(i) - 'a']++;
-            freq[s.charAt(i) - 'a']--;
+
+        for (int i = 0; i < p.length(); ++i) {
+            --freq[p.charAt(i) - 'a'];
+            ++freq[s.charAt(i) - 'a'];
         }
 
-        int l = 0;
-        if (isAnagram(freq)) res.add(0);
-        for (int r = p.length(); r < s.length(); r++) {
-            freq[s.charAt(l++) - 'a']++;
-            freq[s.charAt(r) - 'a']--;
-            if (isAnagram(freq)) res.add(l);
+        if (isZero(freq)) res.add(0);
+
+        for (int i = p.length(); i < s.length(); ++i) {
+            char r = s.charAt(i);
+            char l = s.charAt(i - p.length());
+            
+            --freq[l - 'a'];
+            ++freq[r - 'a'];
+
+            if (isZero(freq)) {
+                res.add(i - p.length() + 1);
+            }
         }
+
         return res;
     }
 
-    private boolean isAnagram(int[] freq) {
-        for (int i : freq) {
+    private boolean isZero(int[] arr) {
+        for (int i : arr) {
             if (i != 0) return false;
         }
         return true;
