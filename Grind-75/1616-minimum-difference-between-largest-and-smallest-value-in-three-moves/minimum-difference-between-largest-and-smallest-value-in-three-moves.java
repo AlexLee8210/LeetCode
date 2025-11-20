63bb31f2034ebf57b287faffcb857bbb9b287220
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-11-20 12:34:41 -0600

    Time: 39 ms (5.17%) | Memory: 65.3 MB (9.48%) - LeetSync

--- Grind-75/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
+++ Grind-75/1616-minimum-difference-between-largest-and-smallest-value-in-three-moves/minimum-difference-between-largest-and-smallest-value-in-three-moves.java
@@ -1,15 +1,38 @@
 class Solution {
     public int minDifference(int[] nums) {
         if (nums.length <= 4) return 0;
-        Arrays.sort(nums);
-        int min = Integer.MAX_VALUE;
-        for (int i = 0; i <= 3; ++i) {
-            int imax = nums.length - (3 - i) - 1;
-            int diff = nums[imax] - nums[i];
-            if (diff < min) {
-                min = diff;
+
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        for (int i = 0; i < nums.length; ++i) {
+            minHeap.offer(nums[i]);
+            if (minHeap.size() > 4) {
+                minHeap.poll();
             }
         }
-        return min;
+
+        PriorityQueue<Integer> maxHeap = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; ++i) {
+            maxHeap.offer(nums[i]);
+            if (maxHeap.size() > 4) {
+                maxHeap.poll();
+            }
+        }
+
+        int[] minElems = new int[4];
+        for (int i = 0; i < 4; ++i) {
+            minElems[i] = minHeap.poll();
+        }
+
+        int[] maxElems = new int[4];
+        for (int i = 3; i >= 0; --i) {
+            maxElems[i] = maxHeap.poll();
+        }
+
+        int minDiff = Integer.MAX_VALUE;
+        for (int i = 0; i < 4; ++i) {
+            int diff = maxElems[i] - minElems[i];
+            minDiff = Math.min(diff, minDiff);
+        }
+        return minDiff;
     }
 }
