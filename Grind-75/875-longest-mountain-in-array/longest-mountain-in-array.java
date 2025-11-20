cb8b42c891f1f494eb4e5b6936c1e33e60d9d760
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-11-20 16:15:05 -0600

    Time: 2 ms (99.70%) | Memory: 47.6 MB (11.18%) - LeetSync

--- Grind-75/875-longest-mountain-in-array/longest-mountain-in-array.java
+++ Grind-75/875-longest-mountain-in-array/longest-mountain-in-array.java
@@ -1,36 +1,25 @@
 class Solution {
     public int longestMountain(int[] arr) {
-        if (arr.length < 3) return 0;
-        int mtnState = 0; // 0: nothing, 1: up, 2: down
-        int start = -1;
-        int maxLen = 0;
-        for (int i = 1; i < arr.length; ++i) {
-            if (mtnState == 0) {
-                if (arr[i] > arr[i - 1]) {
-                    mtnState = 1;
-                    start = i - 1;
-                }
-            } else if (mtnState == 1) {
-                if (arr[i] < arr[i - 1]) {
-                    mtnState = 2;
-                } else if (arr[i] == arr[i - 1]) {
-                    start = -1;
-                    mtnState = 0;
-                }
-            } else if (mtnState == 2) {
-                if (arr[i] >= arr[i - 1]) {
-                    maxLen = Math.max(maxLen, i - start);
-                    mtnState = 0;
-                    if (arr[i] > arr[i - 1]) {
-                        start = i - 1;
-                        mtnState = 1;
-                    }
-                }
-            }
-        }
+        int n = arr.length;
+        if (n < 3) return 0;
 
-        if (mtnState == 2) {
-            maxLen = Math.max(maxLen, arr.length - start);
+        int maxLen = 0;
+        int i = 1;
+        while (i < n) {
+            int inc = 0, dec = 0;
+            while (i < n && arr[i - 1] < arr[i]) {
+                ++i;
+                ++inc;
+            }
+            while (i < n && arr[i - 1] > arr[i]) {
+                ++i;
+                ++dec;
+            }
+            if (inc > 0 && dec > 0) {
+                maxLen = Math.max(maxLen, inc + dec + 1);
+            }
+
+            while (i < n && arr[i - 1] == arr[i]) ++i;
         }
 
         return maxLen;
