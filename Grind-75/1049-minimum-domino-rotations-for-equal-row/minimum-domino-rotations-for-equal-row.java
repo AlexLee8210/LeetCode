cff7ade17c3aeae0c482caf5bec7d875fe17248d
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-11-20 13:20:07 -0600

    Time: 3 ms (96.62%) | Memory: 52.9 MB (6.34%) - LeetSync

--- Grind-75/1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
+++ Grind-75/1049-minimum-domino-rotations-for-equal-row/minimum-domino-rotations-for-equal-row.java
@@ -1,25 +1,18 @@
 class Solution {
     public int minDominoRotations(int[] tops, int[] bottoms) {
-        int n = tops.length;
-        int[] tfreq = new int[7];
-        int[] bfreq = new int[7];
-        int[] doubles = new int[7];
+        int topSwaps = minSwaps(tops, bottoms, tops[0]);
+        if (topSwaps != -1) return topSwaps;
+        return minSwaps(tops, bottoms, bottoms[0]);
+    }
 
-        for (int i = 0; i < n; ++i) {
-            if (tops[i] == bottoms[i]) {
-                ++doubles[tops[i]];
-            }
-            ++tfreq[tops[i]];
-            ++bfreq[bottoms[i]];
+    private int minSwaps(int[] tops, int[] bottoms, int tgt) {
+        int top = 0;
+        int bot = 0;
+        for (int i = 0; i < tops.length; ++i) {
+            if (tops[i] != tgt && bottoms[i] != tgt) return -1;
+            if (tops[i] != tgt) ++top;
+            if (bottoms[i] != tgt) ++bot;
         }
-
-        int min = Integer.MAX_VALUE;
-        for (int i = 1; i <= 6; ++i) {
-            if (n <= bfreq[i] + tfreq[i] - doubles[i]) {
-                min = Math.min(min, Math.min(n - bfreq[i], n - tfreq[i]));
-            }
-        }
-
-        return min == Integer.MAX_VALUE ? -1 : min;
+        return Math.min(top, bot);
     }
 }
