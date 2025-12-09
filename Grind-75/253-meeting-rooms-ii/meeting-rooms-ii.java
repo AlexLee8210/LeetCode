da3669e100953b0d1188b7af7497c18f8e94ab5f
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-09 13:14:48 -0600

    Time: 10 ms (22.08%) | Memory: 48.6 MB (20.28%) - LeetSync

---
+++ Grind-75/253-meeting-rooms-ii/meeting-rooms-ii.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        int n = intervals.length;
+        int[] start = new int[n];
+        int[] end = new int[n];
+
+        for (int i = 0; i < n; ++i) {
+            start[i] = intervals[i][0];
+            end[i] = intervals[i][1];
+        }
+
+        Arrays.sort(start);
+        Arrays.sort(end);
+
+        int rooms = 0;
+        int j = 0;
+        for (int i = 0; i < n; ++i) {
+            if (start[i] < end[j]) {
+                ++rooms;
+            } else {
+                ++j;
+            }
+        }
+
+        return rooms;
+    }
+}
