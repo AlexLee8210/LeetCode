ff6848a146fd1acbbad5635d2377e89527ced954
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-26 13:24:56 -0600

    Time: 2 ms (93.38%) | Memory: 72.1 MB (22.44%) - LeetSync

--- Grind-75/238-product-of-array-except-self/product-of-array-except-self.java
+++ Grind-75/238-product-of-array-except-self/product-of-array-except-self.java
@@ -1,17 +1,20 @@
 class Solution {
     public int[] productExceptSelf(int[] nums) {
-        int[] answer = new int[nums.length];
-        answer[0] = 1;
+        int[] res = new int[nums.length];
+        res[0] = 1;
         for (int i = 0; i < nums.length - 1; ++i) {
-            answer[i + 1] = answer[i] * nums[i];
-        }
-        
-        int prod = 1;
-        for (int i = nums.length - 1; i > 0; --i) {
-            prod *= nums[i];
-            answer[i - 1] *= prod;
+            res[i + 1] = res[i] * nums[i];
         }
 
-        return answer;
+        // 1, 1, 2, 6
+        // 24, 12, 4, 1
+
+        int prod = 1;
+        for (int i = nums.length - 2; i >= 0; --i) {
+            prod *= nums[i + 1];
+            res[i] *= prod;
+        }
+
+        return res;
     }
 }
