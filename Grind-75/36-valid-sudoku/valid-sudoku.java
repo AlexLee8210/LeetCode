affc1326f29eb1502b399aea044856173aa94c2c
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-26 13:54:26 -0600

    Time: 3 ms (52.01%) | Memory: 46.5 MB (34.81%) - LeetSync

--- Grind-75/36-valid-sudoku/valid-sudoku.java
+++ Grind-75/36-valid-sudoku/valid-sudoku.java
@@ -1,25 +1,36 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        boolean valid = true;
-        HashSet<Character> row, col, grid;
-        for (int r = 0; r < board.length; r++) {
-            row = new HashSet<>();
-            col = new HashSet<>();
-            grid = new HashSet<>();
-            for (int c = 0; c < board.length; c++) {
-                if (board[r][c] != '.' && !row.add(board[r][c])) {
-                    return false;
-                }
-                if (board[c][r] != '.' && !col.add(board[c][r])) {
-                    return false;
-                }
-                int rIndex = (r / 3) * 3 + c / 3;
-                int cIndex = (r % 3) * 3 + c % 3;
-                if (board[rIndex][cIndex] != '.' && !grid.add(board[rIndex][cIndex])) {
-                    return false;
-                }
+        // Set<Character> h = new HashSet<>();
+        Set<Character>[] boxes = new Set[9];
+        Set<Character>[] cols = new Set[9];
+        for (int i = 0; i < 9; ++i) {
+            boxes[i] = new HashSet<>();
+            cols[i] = new HashSet<>();
+        }
+        for (int r = 0; r < board.length; ++r) {
+            Set<Character> row = new HashSet<>();
+            for (int c = 0; c < board[0].length; ++c) {
+                if (board[r][c] == '.') continue;
+                if (row.contains(board[r][c])) return false;
+                row.add(board[r][c]);
+
+                if (cols[c].contains(board[r][c])) return false;
+                cols[c].add(board[r][c]);
+
+                int box = cellToBox(r, c);
+                if (boxes[box].contains(board[r][c])) return false;
+                boxes[box].add(board[r][c]);
             }
         }
         return true;
+    }
+
+    private int cellToBox(int r, int c) {
+        // 0,0 -> 0
+        // 0,3 -> 1
+        // 2,3 -> 1
+        // 5,5 -> 4
+        // r/3 * 3 + c/3
+        return (r / 3) * 3 + c / 3;
     }
 }
