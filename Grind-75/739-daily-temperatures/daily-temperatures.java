1fce4485f82486118a455be0135cd629b8ea78c2
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-26 17:16:48 -0600

    Time: 60 ms (77.56%) | Memory: 103.1 MB (84.58%) - LeetSync

--- Grind-75/739-daily-temperatures/daily-temperatures.java
+++ Grind-75/739-daily-temperatures/daily-temperatures.java
@@ -1,15 +1,16 @@
 class Solution {
     public int[] dailyTemperatures(int[] temperatures) {
-        int[] ans = new int[temperatures.length];
-        Stack<int[]> stack = new Stack<>();
-        stack.push(new int[]{temperatures[0], 0});
-        for (int i = 1; i < temperatures.length; i++) {
+        int[] res = new int[temperatures.length];
+        Stack<int[]> stack = new Stack<>(); // [temp, idx]
+        for (int i = 0; i < temperatures.length; ++i) {
             while (!stack.isEmpty() && stack.peek()[0] < temperatures[i]) {
-                int idx = stack.pop()[1];
-                ans[idx] = i - idx;
+                int j = stack.pop()[1];
+                res[j] = i - j;
             }
+
             stack.push(new int[]{temperatures[i], i});
         }
-        return ans;
+
+        return res;
     }
 }
