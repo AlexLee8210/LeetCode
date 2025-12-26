62d839cf90398de6fd9791ce257206feb44fc6ae
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-26 12:56:55 -0600

    Time: 13 ms (79.29%) | Memory: 47.5 MB (65.98%) - LeetSync

--- Grind-75/347-top-k-frequent-elements/top-k-frequent-elements.java
+++ Grind-75/347-top-k-frequent-elements/top-k-frequent-elements.java
@@ -1,23 +1,43 @@
 class Solution {
     public int[] topKFrequent(int[] nums, int k) {
         Map<Integer, Integer> freq = new HashMap<>();
-        int[] res = new int[k];
-        // Heap: O(n + nlogk)
         for (int n : nums) {
             freq.put(n, freq.getOrDefault(n, 0) + 1);
         }
-        PriorityQueue<Integer> heap = new PriorityQueue<>(k + 1, (a,b) -> Integer.compare(freq.get(a), freq.get(b)));
+        int[] res = new int[k];
+        
+        // Heap: O(n + nlogk)
+        // PriorityQueue<Integer> heap = new PriorityQueue<>(k + 1, (a,b) -> Integer.compare(freq.get(a), freq.get(b)));
+        // for (int n : freq.keySet()) {
+        //     heap.add(n);
+        //     if (heap.size() > k) {
+        //         heap.poll();
+        //     }
+        // }
+        // for (int i = 0; i < k; ++i) {
+        //     res[i] = heap.poll();
+        // }
+        // return res;
+
+        // Bucket: O(n + k)
+        List<Integer>[] bucket = new List[nums.length + 1];
         for (int n : freq.keySet()) {
-            heap.add(n);
-            if (heap.size() > k) {
-                heap.poll();
+            int f = freq.get(n);
+            if (bucket[f] == null) {
+                bucket[f] = new ArrayList<>();
+            }
+            bucket[f].add(n);
+        }
+
+        int idx = 0;
+        for (int i = nums.length; i >= 0 && idx < k; --i) {
+            if (bucket[i] == null) continue;
+            int j = 0;
+            while (idx < k && j < bucket[i].size()) {
+                res[idx++] = bucket[i].get(j++);
             }
         }
-        for (int i = 0; i < k; ++i) {
-            res[i] = heap.poll();
-        }
+
         return res;
-        // Bucket: O(n + k)
-        
     }
 }
