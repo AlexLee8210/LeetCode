a1e8d53134fbaf67fe313b29bba9052a2c99b95e
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-26 14:12:18 -0600

    Time: 3 ms (52.01%) | Memory: 46.7 MB (27.42%) - LeetSync

--- Grind-75/36-valid-sudoku/valid-sudoku.java
+++ Grind-75/36-valid-sudoku/valid-sudoku.java
@@ -1,38 +1,33 @@
 class Solution {
     public boolean isValidSudoku(char[][] board) {
-        // Set<Character> h = new HashSet<>();
-        Set<Character>[] boxes = new Set[9];
-        for (int i = 0; i < 9; ++i) {
-            boxes[i] = new HashSet<>();
-        }
         for (int r = 0; r < board.length; ++r) {
             Set<Character> row = new HashSet<>();
             Set<Character> col = new HashSet<>();
+            Set<Character> box = new HashSet<>();
             for (int c = 0; c < board[0].length; ++c) {
                 if (board[r][c] != '.') {
                     if (row.contains(board[r][c])) return false;
                     row.add(board[r][c]);
-
-                    int box = cellToBox(r, c);
-                    if (boxes[box].contains(board[r][c])) return false;
-                    boxes[box].add(board[r][c]);
                 }
 
                 if (board[c][r] != '.') {
                     if (col.contains(board[c][r])) return false;
                     col.add(board[c][r]);
                 }
+
+                // 3,5 -> 3,2
+                // 4,1 -> 3,4
+                // r: r / 3 + c / 3
+                // c: c % 3 + r % 3 * 3
+                int boxr = (r / 3) * 3 + c / 3;
+                int boxc = c % 3 + (r % 3) * 3;
+                // System.out.println(r + "," + c + " -> " + boxr + "," + boxc);
+                if (board[boxr][boxc] != '.') {
+                    if (box.contains(board[boxr][boxc])) return false;
+                    box.add(board[boxr][boxc]);
+                }
             }
         }
         return true;
-    }
-
-    private int cellToBox(int r, int c) {
-        // 0,0 -> 0
-        // 0,3 -> 1
-        // 2,3 -> 1
-        // 5,5 -> 4
-        // r/3 * 3 + c/3
-        return (r / 3) * 3 + c / 3;
     }
 }
