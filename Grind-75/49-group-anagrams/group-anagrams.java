05785729be35d11396fc624dc45f796083749662
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-26 12:34:01 -0600

    Time: 10 ms (34.36%) | Memory: 50 MB (23.08%) - LeetSync

--- Grind-75/49-group-anagrams/group-anagrams.java
+++ Grind-75/49-group-anagrams/group-anagrams.java
@@ -1,29 +1,33 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-        HashMap<String, List<String>> map = new HashMap<>();
-        for (String k : strs) {
-            String key = getKey(k);
-            if (map.containsKey(key)) {
-                map.get(key).add(k);
-            } else {
-                ArrayList<String> list = new ArrayList<>();
-                list.add(k);
-                map.put(key, list);
-            }
-        }
         List<List<String>> res = new ArrayList<>();
-        for (String key : map.keySet()) {
-            res.add(map.get(key));
+        Map<String, List<String>> map = new HashMap<>();
+        for (String str : strs) {
+            String key = toKey(str);
+            if (!map.containsKey(key)) {
+                List<String> list = new ArrayList<>();
+                map.put(key, list);
+                res.add(list);
+            }
+            List<String> anagrams = map.get(key);
+            anagrams.add(str);
         }
+
         return res;
     }
 
-    private String getKey(String k) {
-        char[] chars = new char[26];
-        for (char c : k.toCharArray()) {
-            chars[c - 'a']++;
+    private String toKey(String word) {
+        int[] freq = new int[26];
+        for (char c : word.toCharArray()) {
+            ++freq[c - 'a'];
         }
-        String key = new String(chars);
-        return key;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < 26; ++i) {
+            if (freq[i] != 0) {
+                sb.append((char) i + 'a').append(freq[i]);
+            }
+        }
+
+        return sb.toString();
     }
 }
