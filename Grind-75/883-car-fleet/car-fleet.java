9ed9e833d99e9b62f504902caa0b75956a8bb638
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-26 17:33:00 -0600

    Time: 31 ms (97.15%) | Memory: 161.5 MB (5.06%) - LeetSync

--- Grind-75/883-car-fleet/car-fleet.java
+++ Grind-75/883-car-fleet/car-fleet.java
@@ -1,17 +1,20 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
+        int n = position.length;
         double[] times = new double[target];
-        for (int i = 0; i < speed.length; i++) {
-            times[position[i]] = (double)(target - position[i]) / speed[i];
+        for (int i = 0; i < n; ++i) {
+            times[position[i]] = (double) (target - position[i]) / speed[i];
         }
+
         int cnt = 0;
-        double prevTime = 0;
-        for (int i = target - 1; i >= 0; i--) {
-            if (times[i] > prevTime) {
-                cnt++;
-                prevTime = times[i];
+        double prev = 0;
+        for (int i = target - 1; i >= 0; --i) {
+            if (times[i] > prev) {
+                ++cnt;
+                prev = times[i];
             }
         }
+
         return cnt;
     }
 }
