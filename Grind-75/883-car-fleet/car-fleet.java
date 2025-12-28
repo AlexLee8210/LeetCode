e7d5bb1bd449685741bfd18e54403125ca7ffe0b
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-27 18:06:59 -0700

    Time: 98 ms (25.35%) | Memory: 105.9 MB (23.69%) - LeetSync

--- Grind-75/883-car-fleet/car-fleet.java
+++ Grind-75/883-car-fleet/car-fleet.java
@@ -1,20 +1,26 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
         int n = position.length;
-        double[] times = new double[target];
+        int[][] pairs = new int[n][];
         for (int i = 0; i < n; ++i) {
-            times[position[i]] = (double) (target - position[i]) / speed[i];
+            pairs[i] = new int[]{position[i], speed[i]};
         }
 
-        int cnt = 0;
-        double prev = 0;
-        for (int i = target - 1; i >= 0; --i) {
-            if (times[i] > prev) {
-                ++cnt;
-                prev = times[i];
+        Arrays.sort(pairs, (a, b) -> Integer.compare(a[0], b[0]));
+
+        // 0  3  5  8  10
+        // 1  3  1  4  2
+        // 12 3  7  1  1
+
+        Stack<Double> stack = new Stack<>();
+        for (int[] pair : pairs) {
+            double time = (double) (target - pair[0]) / pair[1];
+            while (!stack.isEmpty() && time >= stack.peek()) {
+                stack.pop();
             }
+            stack.push(time);
         }
 
-        return cnt;
+        return stack.size();
     }
 }
