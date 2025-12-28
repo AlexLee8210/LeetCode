4b17aa7908f0cc9ed90f8c13a8ef9eb1a9521674
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-27 18:33:35 -0700

    Time: 29 ms (89.54%) | Memory: 59.2 MB (41.82%) - LeetSync

--- Grind-75/15-3sum/3sum.java
+++ Grind-75/15-3sum/3sum.java
@@ -1,25 +1,28 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        Arrays.sort(nums);
         List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        int prev = nums[0] - 1;
         for (int i = 0; i < nums.length; ++i) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            if (nums[i] == prev) continue;
             int target = -nums[i];
             int l = i + 1, r = nums.length - 1;
             while (l < r) {
                 int sum = nums[l] + nums[r];
-                if (sum > target) {
-                    --r;
-                } else if (sum < target) {
+                if (sum < target) {
                     ++l;
-                } else {
-                    res.add(Arrays.asList(nums[i], nums[l], nums[r]));
+                } else if (sum > target) {
                     --r;
-                    do {
-                        ++l;
-                    } while (l < r && nums[l - 1] == nums[l]);
+                } else {
+                    res.add(List.of(nums[i], nums[l], nums[r]));
+                    while (l < r && nums[l] == nums[l + 1]) ++l;
+                    while (l < r && nums[r] == nums[r - 1]) --r;
+                    ++l;
+                    --r;
                 }
             }
+
+            prev = nums[i];
         }
 
         return res;
