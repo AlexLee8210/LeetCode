0535658b656d815feb8a6414a4635840d4f76eef
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-28 21:30:18 -0700

    Time: 0 ms (100.00%) | Memory: 48.4 MB (45.99%) - LeetSync

--- Grind-75/792-binary-search/binary-search.java
+++ Grind-75/792-binary-search/binary-search.java
@@ -3,10 +3,10 @@ class Solution {
         int l = 0, r = nums.length - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if (target < nums[m]) {
+            if (nums[m] < target) {
+                l = m + 1;
+            } else if (nums[m] > target) {
                 r = m - 1;
-            } else if (target > nums[m]) {
-                l = l + 1;
             } else {
                 return m;
             }
