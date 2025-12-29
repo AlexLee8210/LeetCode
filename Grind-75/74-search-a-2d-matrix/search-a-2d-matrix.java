d489a945c6332771805a98cae0a88d523da7156f
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-28 21:49:24 -0700

    Time: 0 ms (100.00%) | Memory: 44.3 MB (6.35%) - LeetSync

--- Grind-75/74-search-a-2d-matrix/search-a-2d-matrix.java
+++ Grind-75/74-search-a-2d-matrix/search-a-2d-matrix.java
@@ -1,15 +1,28 @@
 class Solution {
     public boolean searchMatrix(int[][] matrix, int target) {
         int m = matrix.length, n = matrix[0].length;
-        int l = 0, r = m * n;
+        int l = 0, r = m - 1;
+        int row = 0;
+
         while (l <= r) {
             int mid = l + (r - l) / 2;
-            int row = mid / n;
-            int col = mid % n;
-            if (row >= m || col >= n) return false;
-            if (matrix[row][col] < target) {
+            if (matrix[mid][0] < target) {
                 l = mid + 1;
-            } else if (matrix[row][col] > target) {
+                row = mid;
+            } else if (matrix[mid][0] > target) {
+                r = mid - 1;
+            } else {
+                return true;
+            }
+        }
+        
+        l = 0;
+        r = n - 1;
+        while (l <= r) {
+            int mid = l + (r - l) / 2;
+            if (matrix[row][mid] < target) {
+                l = mid + 1;
+            } else if (matrix[row][mid] > target) {
                 r = mid - 1;
             } else {
                 return true;
