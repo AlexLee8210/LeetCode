d786707456baba6b3b3bd4cd1ac3369aad9af400
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-28 22:48:31 -0700

    Time: 0 ms (100.00%) | Memory: 44 MB (7.89%) - LeetSync

--- Grind-75/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
+++ Grind-75/33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.java
@@ -3,21 +3,33 @@ class Solution {
         int l = 0, r = nums.length - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if (nums[m] == target) return m;
-            else if (nums[l] <= nums[m]) {
-                if (nums[l] <= target && target < nums[m]) {
-                    r = m - 1;
-                } else {
+            if (nums[m] > nums[r]) {
+                if (nums[m] < target) {
                     l = m + 1;
+                } else if (nums[m] > target) {
+                    if (target <= nums[r]) {
+                        l = m + 1;
+                    } else {
+                        r = m - 1;
+                    }
+                } else {
+                    return m;
                 }
             } else {
-                if (nums[m] < target && target <= nums[r]) {
-                    l = m + 1;
-                } else {
+                if (target < nums[m]) {
                     r = m - 1;
+                } else if (target > nums[m]) {
+                    if (target <= nums[r]) {
+                        l = m + 1;
+                    } else {
+                        r = m - 1;
+                    }
+                } else {
+                    return m;
                 }
             }
         }
+
         return -1;
     }
 }
