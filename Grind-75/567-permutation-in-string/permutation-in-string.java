eac691c8fb8aac2bd090379f7e58187f0e46a143
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-30 00:01:47 -0700

    Time: 7 ms (68.70%) | Memory: 44.2 MB (50.03%) - LeetSync

--- Grind-75/567-permutation-in-string/permutation-in-string.java
+++ Grind-75/567-permutation-in-string/permutation-in-string.java
@@ -1,41 +1,43 @@
 class Solution {
     public boolean checkInclusion(String s1, String s2) {
-        if (s1.length() > s2.length()) return false;
-        int[] tgt = new int[26];
+        if (s2.length() < s1.length()) return false;
+        int n = s1.length();
         int[] freq = new int[26];
-        char[] chars = s2.toCharArray();
-        int l = 0, r = 0;
-        int matches = 0;
+        int match = 26;
         for (char c : s1.toCharArray()) {
-            tgt[c - 'a']++;
-            freq[chars[r++] - 'a']++;
+            if (freq[c - 'a']++ == 0) {
+                --match;
+            }
+        }
+
+        for (int i = 0; i < n; ++i) {
+            char c = s2.charAt(i);
+            --freq[c - 'a'];
+            if (freq[c - 'a'] == 0) {
+                ++match;
+            } else if (freq[c - 'a'] == -1) {
+                --match;
+            }
         }
         
-        for (int i = 0; i < tgt.length; i++) {
-            if (freq[i] == tgt[i]) {
-                matches++;
+        if (match == 26) return true;
+
+        for (int i = n; i < s2.length(); ++i) {
+            if (++freq[s2.charAt(i - n) - 'a'] == 0) {
+                ++match;
+            } else if (freq[s2.charAt(i - n) - 'a'] == 1) {
+                --match;
             }
-        }
-        if (matches == 26) {
-            return true;
-        }
-        for (int i = r; i < s2.length(); i++) {
-            freq[chars[l] - 'a']--;
-            if (freq[chars[l] - 'a'] == tgt[chars[l] - 'a']) {
-                matches++;
-            } else if (freq[chars[l] - 'a'] + 1 == tgt[chars[l] - 'a']) {
-                matches--;
+            
+            if (--freq[s2.charAt(i) - 'a'] == 0) {
+                ++match;
+            } else if (freq[s2.charAt(i) - 'a'] == -1) {
+                --match;
             }
 
-            freq[chars[i] - 'a']++;
-            if (freq[chars[i] - 'a'] == tgt[chars[i] - 'a']) {
-                matches++;
-            } else if (freq[chars[i] - 'a'] - 1 == tgt[chars[i] - 'a']) {
-                matches--;
-            }
-            if (matches == 26) return true;
-            l++;
+            if (match == 26) return true;
         }
+
         return false;
     }
 }
