a559a6b6188f5127b99fb25677df59f3ad85d989
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-31 17:32:21 -0700

    Time: 17 ms (57.26%) | Memory: 47.9 MB (23.30%) - LeetSync

--- Grind-75/76-minimum-window-substring/minimum-window-substring.java
+++ Grind-75/76-minimum-window-substring/minimum-window-substring.java
@@ -1,39 +1,47 @@
 class Solution {
     public String minWindow(String s, String t) {
-        HashMap<Character, Integer> map = new HashMap<>();
-        HashMap<Character, Integer> tgt = new HashMap<>();
-        StringBuilder sb = new StringBuilder();
-        int matches = 0;
-        String res = "";
+        if (t.length() > s.length()) return "";
 
+        Map<Character, Integer> sFreq = new HashMap<>();
+        Map<Character, Integer> tFreq = new HashMap<>();
         for (char c : t.toCharArray()) {
-            tgt.put(c, tgt.getOrDefault(c, 0) + 1);
+            tFreq.put(c, tFreq.getOrDefault(c, 0) + 1);
         }
 
-        for (int i = 0; i < s.length(); i++) {
-            int freq = map.getOrDefault(s.charAt(i), 0) + 1;
-            map.put(s.charAt(i), freq);
-            sb.append(s.charAt(i));
-
-            if (!tgt.containsKey(s.charAt(i))) continue;
-
-            int tgtFreq = tgt.get(s.charAt(i));
-            if (freq == tgtFreq) {
-                matches++;
+        int[] res = new int[]{-1, Integer.MAX_VALUE - 1};
+        int matches = 0;
+        int l = 0;
+        for (int r = 0; r < s.length(); ++r) {
+            char c = s.charAt(r);
+            if (!tFreq.containsKey(c)) continue;
+            int newf = sFreq.getOrDefault(c, 0) + 1;
+            int matchf = tFreq.get(c);
+            sFreq.put(c, newf);
+            if (newf == matchf) {
+                ++matches;
             }
 
-            while (matches == tgt.size()) {
-                if (res.length() == 0 || sb.length() < res.length()) {
-                    res = sb.toString();
+            while (matches >= tFreq.size()) {
+                char lc = s.charAt(l);
+                if (!tFreq.containsKey(lc)) {
+                    ++l;
+                    continue;
                 }
-                freq = map.get(sb.charAt(0)) - 1;
-                map.put(sb.charAt(0), freq);
-                if (tgt.containsKey(sb.charAt(0)) && freq < tgt.get(sb.charAt(0))) {
-                    matches--;
+
+                if (res[1] - res[0] > r - l) {
+                    res[0] = l;
+                    res[1] = r;
                 }
-                sb.deleteCharAt(0);
+
+                sFreq.put(lc, sFreq.get(lc) - 1);
+                ++l;
+
+                if (sFreq.get(lc) == tFreq.get(lc) - 1) {
+                    --matches;
+                }
             }
         }
-        return res;
+
+        return res[0] == -1 ? "" : s.substring(res[0], res[1] + 1);
     }
 }
