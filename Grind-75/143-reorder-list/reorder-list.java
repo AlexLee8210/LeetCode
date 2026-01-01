89614ad929e849af4fecc2949be19d2dcdabd95c
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2025-12-31 18:24:36 -0700

    Time: 48 ms (5.31%) | Memory: 50.6 MB (6.62%) - LeetSync

---
+++ Grind-75/143-reorder-list/reorder-list.java
@@ -0,0 +1,49 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        if (head == null || head.next == null) return;
+        
+        ListNode slow = head, fast = head, prev = null;
+        while (fast != null && fast.next != null) {
+            prev = slow;
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        prev.next = null;
+        prev = null;
+        while (slow != null) {
+            ListNode next = slow.next;
+            slow.next = prev;
+            prev = slow;
+            slow = next;
+        }
+
+        ListNode tmp = prev;
+        while (tmp != null) {
+            System.out.println(tmp.val);
+            tmp = tmp.next;
+        }
+        
+        // len(cur2) >= len(cur1)
+        ListNode cur1 = head;
+        ListNode cur2 = prev;
+        while (cur1 != null) {
+            ListNode next1 = cur1.next;
+            ListNode next2 = cur2.next;
+            cur1.next = cur2;
+            cur2.next = next2 != null && next1 == null ? next2 : next1;
+            cur2 = next2;
+            cur1 = next1;
+        }
+    }
+}
