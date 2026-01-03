1deb898002219447bd52e16ed9e2f0d755178d11
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-03 16:00:40 -0700

    Time: 2 ms (95.36%) | Memory: 45.9 MB (34.76%) - LeetSync

--- Grind-75/39-combination-sum/combination-sum.java
+++ Grind-75/39-combination-sum/combination-sum.java
@@ -1,20 +1,20 @@
 class Solution {
     public List<List<Integer>> combinationSum(int[] candidates, int target) {
-        // Arrays.sort(candidates);
         List<List<Integer>> res = new ArrayList<>();
-        helper(candidates, 0, target, new ArrayList<>(), res);
+        helper(candidates, target, 0, new ArrayList<>(), res);
         return res;
     }
 
-    private void helper(int[] nums, int index, int target, List<Integer> cur, List<List<Integer>> res) {
-        if (target <= 0 || index >= nums.length) {
-            if (target == 0) res.add(new ArrayList<>(cur));
+    private void helper(int[] candidates, int target, int idx, List<Integer> cur, List<List<Integer>> res) {
+        if (target == 0) {
+            res.add(new ArrayList<>(cur));
             return;
         }
+        if (idx >= candidates.length || target < 0) return;
 
-        for (int i = index; i < nums.length; ++i) {
-            cur.add(nums[i]);
-            helper(nums, i, target - nums[i], cur, res);
+        for (int i = idx; i < candidates.length; ++i) {
+            cur.add(candidates[i]);
+            helper(candidates, target - candidates[i], i, cur, res);
             cur.remove(cur.size() - 1);
         }
     }
