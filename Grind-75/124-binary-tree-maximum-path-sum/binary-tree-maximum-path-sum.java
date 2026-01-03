dd6c70769e15d3fe377eba5c9616564c6b79614b
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-02 19:13:38 -0700

    Time: 2 ms (3.12%) | Memory: 46.7 MB (55.42%) - LeetSync

---
+++ Grind-75/124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public int maxPathSum(TreeNode root) {
+        int[] maxPath = new int[]{Integer.MIN_VALUE};
+        System.out.println(maxPathHelper(root, maxPath));
+        return maxPath[0];
+    }
+
+
+    private int maxPathHelper(TreeNode root, int[] maxPath) {
+        if (root == null) return 0;
+        int left = maxPathHelper(root.left, maxPath);
+        int right = maxPathHelper(root.right, maxPath);
+        
+        int curpath = left + right + root.val;
+        maxPath[0] = Math.max(maxPath[0], curpath);
+        return Math.max(0, Math.max(left, right) + root.val);
+    }
+}
