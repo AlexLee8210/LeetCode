a19969ebce6d791c658fa47a29fbeb38200a3ac1
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-04 11:30:01 -0700

    Time: 8 ms (69.92%) | Memory: 65.3 MB (57.70%) - LeetSync

--- Grind-75/131-palindrome-partitioning/palindrome-partitioning.java
+++ Grind-75/131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,29 +1,31 @@
 class Solution {
     public List<List<String>> partition(String s) {
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        for (int l = s.length() - 1; l >= 0; --l) {
+            dp[l][l] = true;
+            for (int r = l + 1; r < s.length(); ++r) {
+                if (s.charAt(r) != s.charAt(l)) continue;
+                dp[l][r] = (r - l) < 2 || dp[l + 1][r - 1];
+            }
+        }
+
         List<List<String>> res = new ArrayList<>();
-        helper(s, 0, new ArrayList<>(), res);
+        helper(s, 0, new ArrayList<>(), res, dp);
         return res;
     }
 
-    private void helper(String s, int start, List<String> cur, List<List<String>> res) {
+    private void helper(String s, int start, List<String> cur, List<List<String>> res, boolean[][] dp) {
         if (start == s.length()) {
             res.add(new ArrayList<>(cur));
             return;
         }
 
         for (int r = start; r < s.length(); ++r) {
-            if (isPalindrome(s, start, r)) {
+            if (dp[start][r]) {
                 cur.add(s.substring(start, r + 1));
-                helper(s, r + 1, cur, res);
+                helper(s, r + 1, cur, res, dp);
                 cur.remove(cur.size() - 1);
             }
         }
-    }
-
-    private boolean isPalindrome(String s, int l, int r) {
-        while (l < r) {
-            if (s.charAt(l++) != s.charAt(r--)) return false;
-        }
-        return true;
     }
 }
