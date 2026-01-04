8a39db4248118daf215fd352544fe69a45604f9d
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-04 15:45:27 -0700

    Time: 147 ms (5.04%) | Memory: 54.3 MB (56.98%) - LeetSync

--- Grind-75/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ Grind-75/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,19 +1,38 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> maxheap = new PriorityQueue<>(
-            k + 1,
-            (a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1], a[0] * a[0] + a[1] * a[1])
-        );
-
-        for (int[] point : points) {
-            maxheap.offer(point);
-            if (maxheap.size() > k) maxheap.poll();
+        int l = 0, r = points.length - 1;
+        int p = points.length;
+        while (p != k) {
+            p = partition(points, l, r);
+            if (p < k) {
+                l = p + 1;
+            } else if (p > k) {
+                r = p - 1;
+            }
         }
+        return Arrays.copyOf(points, k);
+    }
 
-        int[][] res = new int[k][];
-        for (int i = 0; i < k; ++i) {
-            res[i] = maxheap.poll();
+    private int dist(int[] point) {
+        return point[0] * point[0] + point[1] * point[1];
+    }
+
+    private void swap(int[][] arr, int i, int j) {
+        int[] tmp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = tmp;
+    }
+
+    private int partition(int[][] points, int l, int r) {
+        int pivot = dist(points[r]);
+
+        int i = l;
+        for (int j = l; j < r; ++j) {
+            if (dist(points[j]) < pivot) {
+                swap(points, i++, j);
+            }
         }
-        return res;
+        swap(points, i, r);
+        return i;
     }
 }
