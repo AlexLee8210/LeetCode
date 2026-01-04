87c938abb912a7dc28944b2b385076890bffe8f9
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-04 14:21:43 -0700

    Time: 29 ms (65.51%) | Memory: 58 MB (24.65%) - LeetSync

--- Grind-75/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
+++ Grind-75/1014-k-closest-points-to-origin/k-closest-points-to-origin.java
@@ -1,16 +1,19 @@
 class Solution {
     public int[][] kClosest(int[][] points, int k) {
-        PriorityQueue<int[]> pq = new PriorityQueue<>((int[] a, int[] b) -> b[0] * b[0] + b[1] * b[1] - a[0] * a[0] - a[1] * a[1]);
-        for (int i = 0; i < points.length; ++i) {
-            pq.add(points[i]);
-            if (pq.size() > k) pq.remove();
+        PriorityQueue<int[]> maxheap = new PriorityQueue<>(
+            k + 1,
+            (a, b) -> Integer.compare(b[0] * b[0] + b[1] * b[1], a[0] * a[0] + a[1] * a[1])
+        );
+
+        for (int[] point : points) {
+            maxheap.offer(point);
+            if (maxheap.size() > k) maxheap.poll();
         }
 
-        int[][] res = new int[k][2];
+        int[][] res = new int[k][];
         for (int i = 0; i < k; ++i) {
-            res[i] = pq.remove();
+            res[i] = maxheap.poll();
         }
-
         return res;
     }
 }
