92b366fa1f6f998e10cbd6b6c4718ef0d411c868
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-04 14:02:41 -0700

    Time: 51 ms (5.65%) | Memory: 47.3 MB (5.79%) - LeetSync

--- Grind-75/51-n-queens/n-queens.java
+++ Grind-75/51-n-queens/n-queens.java
@@ -12,11 +12,11 @@ class Solution {
         for (int i = 0; i < n; ++i) {
             Arrays.fill(board[i], '.');
         }
-        helper(n, 0, board, res);
+        helper(n, 0, board, res, new boolean[2 * n], new boolean[2 * n], new boolean[3 * n]);
         return res;
     }
 
-    private void helper(int n, int idx, char[][] board, List<List<String>> res) {
+    private void helper(int n, int idx, char[][] board, List<List<String>> res, boolean[] vcol, boolean[] vposdiag, boolean[] vnegdiag) {
         if (idx == n) {
             System.out.println(res);
             List<String> cur = new ArrayList<>();
@@ -31,29 +31,36 @@ class Solution {
         for (int c = 0; c < n; ++c) {
             if (board[r][c] != '.') continue;
 
-            boolean valid = true;
-            for (int[] dir : dirs) {
-                if (!canPlace(r, c, dir[0], dir[1], board)) {
-                    valid = false;
-                    break;
-                }
-            }
+            boolean valid = !(vcol[c] || vposdiag[r + c] || vnegdiag[r - c + n]);
+            // for (int[] dir : dirs) {
+            //     if (!canPlace(r, c, dir[0], dir[1], board)) {
+            //         valid = false;
+            //         break;
+            //     }
+            // }
+
 
             if (!valid) continue;
             
             board[r][c] = 'Q';
-            helper(n, idx + 1, board, res);
+            vcol[c] = true;
+            vposdiag[r + c] = true;
+            vnegdiag[r - c + n] = true;
+            helper(n, idx + 1, board, res, vcol, vposdiag, vnegdiag);
+            vcol[c] = false;
+            vposdiag[r + c] = false;
+            vnegdiag[r - c + n] = false;
             board[r][c] = '.';
         }
     }
 
-    private boolean canPlace(int r, int c, int dr, int dc, char[][] board) {
-        int n = board.length;
-        while (r >= 0 && r < n && c >= 0 && c < n) {
-            if (board[r][c] == 'Q') return false;
-            r += dr;
-            c += dc;
-        }
-        return true;
-    }
+    // private boolean canPlace(int r, int c, int dr, int dc, char[][] board) {
+    //     int n = board.length;
+    //     while (r >= 0 && r < n && c >= 0 && c < n) {
+    //         if (board[r][c] == 'Q') return false;
+    //         r += dr;
+    //         c += dc;
+    //     }
+    //     return true;
+    // }
 }
