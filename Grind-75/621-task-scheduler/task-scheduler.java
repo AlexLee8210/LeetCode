6a1c2d36bcad3a035a8ac844474c3b3b827e7dd5
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-04 23:08:41 -0700

    Time: 22 ms (54.72%) | Memory: 47.3 MB (98.14%) - LeetSync

--- Grind-75/621-task-scheduler/task-scheduler.java
+++ Grind-75/621-task-scheduler/task-scheduler.java
@@ -2,31 +2,33 @@ class Solution {
     public int leastInterval(char[] tasks, int n) {
         int[] freq = new int[26];
         for (char c : tasks) {
-            freq[c - 'A']++;
+            ++freq[c - 'A'];
         }
 
-        PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> b - a);
-        for (int f : freq) {
-            if (f == 0) continue;
-            pq.offer(f);
+        PriorityQueue<Integer> maxheap = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        Queue<int[]> cooldown = new LinkedList<>(); // [freq, time]
+        for (int i = 0; i < freq.length; ++i) {
+            if (freq[i] > 0) maxheap.offer(freq[i]);
         }
 
         int time = 0;
-        Deque<int[]> dq = new LinkedList<>();
-        while (!pq.isEmpty() || !dq.isEmpty()) {
-            if (!dq.isEmpty() && dq.peekFirst()[1] <= time) {
-                pq.offer(dq.removeFirst()[0]);
+        while (!maxheap.isEmpty() || !cooldown.isEmpty()) {
+            if (!cooldown.isEmpty()) {
+                if (cooldown.peek()[1] == time) {
+                    maxheap.offer(cooldown.poll()[0]);
+                }
             }
 
-            if (!pq.isEmpty()) {
-                int f = pq.poll();
+            if (!maxheap.isEmpty()) {
+                int f = maxheap.poll();
                 if (f > 1) {
-                    dq.addLast(new int[]{f - 1, time + n + 1});
+                    cooldown.offer(new int[]{f - 1, time + n + 1});
                 }
+            } else {
+                time = cooldown.peek()[1] - 1;
             }
             ++time;
         }
-
         return time;
     }
 }
