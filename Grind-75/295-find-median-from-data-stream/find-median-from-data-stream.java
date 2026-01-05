b92076724b9c0e9afda0b83b72000d2717cb0391
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-04 23:49:55 -0700

    Time: 128 ms (76.81%) | Memory: 111.3 MB (86.31%) - LeetSync

--- Grind-75/295-find-median-from-data-stream/find-median-from-data-stream.java
+++ Grind-75/295-find-median-from-data-stream/find-median-from-data-stream.java
@@ -1,39 +1,28 @@
 class MedianFinder {
-    PriorityQueue<Integer> small, large;
+
+    private PriorityQueue<Integer> low;
+    private PriorityQueue<Integer> high;
 
     public MedianFinder() {
-        small = new PriorityQueue<>((a, b) -> b - a);
-        large = new PriorityQueue<>();
+        low = new PriorityQueue<>((a, b) -> Integer.compare(b, a));
+        high = new PriorityQueue<>();
     }
     
     public void addNum(int num) {
-        int smallMax = small.isEmpty() ? Integer.MAX_VALUE : small.peek();
-        // int largeMin = large.isEmpty() ? Integer.MAX_VALUE : large.peek();
-        if (num < smallMax) {
-            small.offer(num);
-        } else {
-            large.offer(num);
+        low.offer(num);
+        if (low.size() - high.size() > 1) {
+            high.offer(low.poll());
         }
-        rebalance();
-    }
-
-    private void rebalance() {
-        if (Math.abs(small.size() - large.size()) <= 1) return;
-        while (small.size() - large.size() > 1) {
-            large.offer(small.poll());
-        }
-        while (large.size() - small.size() >= 1) {
-            small.offer(large.poll());
+        if (!high.isEmpty() && low.peek() > high.peek()) {
+            high.offer(low.poll());
+            if (high.size() > low.size()) low.offer(high.poll());
         }
     }
     
     public double findMedian() {
-        // System.out.println(small);
-        // System.out.println(large);
-        if (small.size() == large.size()) {
-            return (small.peek() + large.peek()) / 2.0;
-        }
-        return small.size() > large.size() ? small.peek() : large.peek();
+        int size = low.size() + high.size();
+        double med = size % 2 == 0 ? (low.peek() + high.peek()) / 2.0 : low.peek();
+        return med;
     }
 }
 
