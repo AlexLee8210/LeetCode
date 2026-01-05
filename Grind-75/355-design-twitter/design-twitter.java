646dd83639607d1d2a91fd67804da09b68a499c5
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-04 23:33:24 -0700

    Time: 32 ms (63.47%) | Memory: 56.4 MB (72.09%) - LeetSync

--- Grind-75/355-design-twitter/design-twitter.java
+++ Grind-75/355-design-twitter/design-twitter.java
@@ -1,23 +1,47 @@
 class Twitter {
     private Map<Integer, Set<Integer>> follows;
-    private List<int[]> tweets;
+    private Map<Integer, List<int[]>> tweets;
+    private int time;
 
     public Twitter() {
         follows = new HashMap<>();
-        tweets = new ArrayList<>();
+        tweets = new HashMap<>();
+        time = 0;
     }
     
     public void postTweet(int userId, int tweetId) {
-        tweets.add(new int[]{userId, tweetId});
+        if (!tweets.containsKey(userId)) {
+            tweets.put(userId, new ArrayList<>());
+            follow(userId, userId);
+        }
+        tweets.get(userId).add(new int[]{time++, tweetId});
     }
     
     public List<Integer> getNewsFeed(int userId) {
         List<Integer> feed = new ArrayList<>(10);
-        Set<Integer> following = follows.getOrDefault(userId, new HashSet<>());
-        for (int i = tweets.size() - 1; i >= 0 && feed.size() < 10; --i) {
-            if (!following.contains(tweets.get(i)[0]) && tweets.get(i)[0] != userId) continue;
-            feed.add(tweets.get(i)[1]);
+        Set<Integer> following = follows.get(userId);
+        if (following == null) return feed;
+
+        PriorityQueue<int[]> minheap = new PriorityQueue<>((a, b) -> Integer.compare(a[0], b[0]));
+
+        for (int followeeId : following) {
+            List<int[]> cur = tweets.get(followeeId);
+            if (cur == null) continue;
+
+            for (int i = cur.size() - 1; i >= 0; --i) {
+                if (minheap.size() == 10 && cur.get(i)[0] < minheap.peek()[0]) break;
+
+                minheap.add(cur.get(i));
+                if (minheap.size() > 10) minheap.poll();
+            }
         }
+
+        while (!minheap.isEmpty()) {
+            feed.add(minheap.poll()[1]);
+        }
+
+        Collections.reverse(feed);
+
         return feed;
     }
     
