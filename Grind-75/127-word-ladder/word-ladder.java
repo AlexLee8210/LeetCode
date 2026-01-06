08256e92626c141bcbda171cf664707adfdb8fb2
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-06 12:23:21 -0700

    Time: 1002 ms (7.73%) | Memory: 46.8 MB (95.39%) - LeetSync

--- Grind-75/127-word-ladder/word-ladder.java
+++ Grind-75/127-word-ladder/word-ladder.java
@@ -1,50 +1,35 @@
 class Solution {
     public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Deque<String> dq = new LinkedList<>();
+        Queue<String> queue = new LinkedList<>();
         Set<String> vis = new HashSet<>();
-        Set<String> wordSet = new HashSet<>(wordList);
-        dq.addLast(beginWord);
-        int dist = 0;
+        queue.offer(beginWord);
 
-        while (!dq.isEmpty()) {
-            ++dist;
-            int size = dq.size();
+        int len = 0;
+        while (!queue.isEmpty()) {
+            ++len;
+            int size = queue.size();
             for (int i = 0; i < size; ++i) {
-                String cur = dq.pollFirst();
-                if (cur.equals(endWord)) return dist;
-                
-                // for (String word : wordList) {
-                //     if (vis.contains(word)) continue;
-                //     if (canTransform(cur, word)) {
-                //         dq.addLast(word);
-                //         vis.add(word);
-                //     }
-                // }
-                char[] newWord = cur.toCharArray();
-                for (int j = 0; j < cur.length(); ++j) {
-                    char tmp = newWord[j];
-                    for (int k = 'a'; k <= 'z'; ++k) {
-                        newWord[j] = (char) k;
-                        String word = new String(newWord);
-                        if (wordSet.contains(word) && !vis.contains(word)) {
-                            dq.addLast(word);
-                            vis.add(word);
-                        }
+                String src = queue.poll();
+                if (src.equals(endWord)) return len;
+                for (String dst : wordList) {
+                    if (vis.contains(dst)) continue;
+                    if (isValidNextWord(src, dst)) {
+                        queue.offer(dst);
+                        vis.add(dst);
                     }
-                    newWord[j] = tmp;
                 }
             }
         }
         return 0;
     }
 
-    private boolean canTransform(String a, String b) {
-        boolean diff = false;
+    private boolean isValidNextWord(String a, String b) {
+        int diff = 0;
         for (int i = 0; i < a.length(); ++i) {
-            if (a.charAt(i) == b.charAt(i)) continue;
-            if (diff) return false;
-            diff = true;
+            if (a.charAt(i) != b.charAt(i)) {
+                if (++diff == 2) return false;
+            }
         }
-        return diff;
+        return true;
     }
 }
