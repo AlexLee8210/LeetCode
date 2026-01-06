cd84776dc679079420a9344fe02180db90f57532
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-05 19:11:45 -0700

    Time: 8 ms (63.97%) | Memory: 48.5 MB (99.12%) - LeetSync

--- Grind-75/286-walls-and-gates/walls-and-gates.java
+++ Grind-75/286-walls-and-gates/walls-and-gates.java
@@ -1,43 +1,42 @@
 class Solution {
-    int[][] dirs = new int[][]{{1, 0}, {0, 1}, {-1, 0}, {0, -1}};
-
+    private final int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
     public void wallsAndGates(int[][] rooms) {
-        for (int r = 0; r < rooms.length; r++) {
-            for (int c = 0; c < rooms[0].length; c++) {
-                if (rooms[r][c] == 0) {
-                    bfs(rooms, r, c);
-                }
+        for (int r = 0; r < rooms.length; ++r) {
+            for (int c = 0; c < rooms[0].length; ++c) {
+                if (rooms[r][c] == 0) bfs(rooms, r, c);
             }
         }
     }
 
-    private void bfs(int[][] rooms, int r, int c) {
-        Queue<int[]> q = new LinkedList<>();
-        q.offer(new int[]{r, c});
+    private void bfs(int[][] rooms, int row, int col) {
+        int m = rooms.length, n = rooms[0].length;
+        Queue<int[]> queue = new LinkedList<>();
+        queue.offer(new int[]{row, col});
         int dist = 0;
 
-        while (!q.isEmpty()) {
-            int size = q.size();
-            dist++;
-            
-            for (int i = 0; i < size; i++) {
-                int[] pos = q.poll();
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+
+            for (int i = 0; i < size; ++i) {
+                int[] pos = queue.poll();
+                int r = pos[0], c = pos[1];
+
+                if (rooms[r][c] < dist) continue;
+                if (rooms[r][c] == dist && dist != 0) continue;
+                rooms[r][c] = dist;
+
                 for (int[] dir : dirs) {
-                    int newR = pos[0] + dir[0];
-                    int newC = pos[1] + dir[1];
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
 
-                    if (!isValid(newR, newC, rooms)) continue;
-                    if (rooms[newR][newC] == -1) continue;
-                    if (rooms[newR][newC] <= dist) continue;
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+                    if (rooms[nr][nc] <= 0) continue;
 
-                    rooms[newR][newC] = dist;
-                    q.offer(new int[]{newR, newC});
+                    queue.offer(new int[]{nr, nc});
                 }
             }
-        }
-    }
 
-    private boolean isValid(int r, int c, int[][] rooms) {
-        return r >= 0 && r < rooms.length && c >= 0 && c < rooms[r].length;
+            ++dist;
+        }
     }
 }
