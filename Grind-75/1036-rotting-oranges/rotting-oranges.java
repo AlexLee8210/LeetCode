04262999dfe8badae3f991106685add5597327d9
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-05 19:56:07 -0700

    Time: 2 ms (90.18%) | Memory: 44.3 MB (27.03%) - LeetSync

--- Grind-75/1036-rotting-oranges/rotting-oranges.java
+++ Grind-75/1036-rotting-oranges/rotting-oranges.java
@@ -1,46 +1,45 @@
 class Solution {
-    private class Point {
-        public int x, y;
-        public Point(int _x, int _y) {
-            x = _x;
-            y = _y;
-        }
-    }
+    private final int[][] dirs = new int[][]{{-1, 0}, {0, -1}, {1, 0}, {0, 1}};
 
     public int orangesRotting(int[][] grid) {
-        int[][] dirs = new int[][]{new int[]{-1, 0}, new int[]{0, -1}, new int[]{1, 0}, new int[]{0, 1}};
-        Queue<Pair<Point, Integer>> q = new LinkedList<>();
         int m = grid.length, n = grid[0].length;
-        int freshCount = 0;
-        for (int r = 0; r < m; r++) {
-            for (int c = 0; c < n; c++) {
-                if (grid[r][c] == 1) freshCount++;
-                if (grid[r][c] == 2) q.offer(new Pair<>(new Point(r, c), 0));
-            }
-        }
-        if (freshCount == 0) return 0;
-        int numRotted = 0;
-        int min = 0;
-        while (!q.isEmpty()) {
-            Pair<Point, Integer> p = q.poll();
-            int r = p.getKey().x;
-            int c = p.getKey().y;
-            int minToRot = p.getValue();
-            min = Math.max(minToRot, min);
-            if (grid[r][c] == 0) continue;
-            for (int[] dir : dirs) {
-                int newR = r + dir[0], newC = c + dir[1];
-                if (isValid(m, n, newR, newC) && grid[newR][newC] == 1) {
-                    grid[newR][newC] = 2;
-                    q.offer(new Pair<>(new Point(newR, newC), minToRot + 1));
-                    numRotted++;
+        int fresh = 0;
+        Queue<int[]> queue = new LinkedList<>();
+        for (int r = 0; r < m; ++r) {
+            for (int c = 0; c < n; ++c) {
+                if (grid[r][c] == 2) {
+                    queue.offer(new int[]{r, c});
+                } else if (grid[r][c] == 1) {
+                    ++fresh;
                 }
             }
         }
-        return numRotted == freshCount ? min : -1;
-    }
 
-    private boolean isValid(int m, int n, int r, int c) {
-        return r >= 0 && c >= 0 && r < m && c < n;
+        int minutes = 0;
+        int min = 0;
+        while (!queue.isEmpty()) {
+            ++minutes;
+            int size = queue.size();
+
+            for (int i = 0; i < size; ++i) {
+                int[] pos = queue.poll();
+                int r = pos[0], c = pos[1];
+
+                for (int[] dir : dirs) {
+                    int nr = r + dir[0];
+                    int nc = c + dir[1];
+
+                    if (nr < 0 || nr >= m || nc < 0 || nc >= n) continue;
+                    if (grid[nr][nc] != 1) continue;
+
+                    grid[nr][nc] = 2;
+                    min = minutes;
+                    --fresh;
+                    queue.offer(new int[]{nr, nc});
+                }
+            }
+        }
+
+        return fresh == 0 ? min : -1;
     }
 }
