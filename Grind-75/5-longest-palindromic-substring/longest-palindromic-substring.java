fb5b0df57c3b6e498340b9c8c5d83a5a1c2e0bef
Alexander Lee <39893161+AlexLee8210@users.noreply.github.com>
2026-01-06 12:38:27 -0700

    Time: 111 ms (31.77%) | Memory: 48.3 MB (8.77%) - LeetSync

--- Grind-75/5-longest-palindromic-substring/longest-palindromic-substring.java
+++ Grind-75/5-longest-palindromic-substring/longest-palindromic-substring.java
@@ -1,25 +1,24 @@
 class Solution {
     public String longestPalindrome(String s) {
-        String res = "";
-
-        for (int i = 0; i < s.length(); i++) {
-            String p1 = palindrome(s, i, i);
-            String p2 = palindrome(s, i, i + 1);
-            if (p1.length() > res.length()) {
-                res = p1;
-            }
-            if (p2.length() > res.length()) {
-                res = p2;
+        int n = s.length();
+        int[] ans = new int[]{-1, -1};
+        int maxLen = -1;
+        boolean[][] dp = new boolean[n][n];
+        for (int i = n - 1; i >= 0; --i) {
+            dp[i][i] = true;
+            for (int j = i; j < n; ++j) {
+                if (s.charAt(i) != s.charAt(j)) continue;
+                if (j - i + 1 > 2) dp[i][j] = dp[i + 1][j - 1];
+                else dp[i][j] = true;
+                if (dp[i][j]) {
+                    if (maxLen < j - i + 1) {
+                        ans[0] = i;
+                        ans[1] = j;
+                        maxLen = j - i + 1;
+                    }
+                }
             }
         }
-        return res;
+        return maxLen == -1 ? "" : s.substring(ans[0], ans[1] + 1);
     }
-
-    private String palindrome(String s, int l, int r) {
-        while (l >= 0 && r < s.length() && s.charAt(l) == s.charAt(r)) {
-            l--;
-            r++;
-        }
-        return s.substring(l + 1, r);
-    } 
 }
